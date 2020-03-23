0918c79e4d3b2c3752697356c47e05eeba0380e8
Sean Anderson <sean@seananderson.ca>
2020-03-23 13:33:59 -0700

    Create noise.py

---
+++ code/noise.py
@@ -0,0 +1,36 @@
+import time
+
+from talon import ctrl
+from talon import tap
+from talon.audio import noise
+from talon.track.geom import Point2d
+
+class NoiseModel:
+    def __init__(self):
+        self.hiss_start = 0
+        self.hiss_last = 0
+        self.button = 0
+        self.mouse_origin = Point2d(0, 0)
+        self.mouse_last = Point2d(0, 0)
+        self.dragging = False
+
+        tap.register(tap.MMOVE, self.on_move)
+        noise.register('noise', self.on_noise)
+
+    def on_move(self, typ, e):
+        if typ != tap.MMOVE: return
+        self.mouse_last = pos = Point2d(e.x, e.y)
+        if self.hiss_start and not self.dragging:
+            if (pos - self.mouse_origin).len() > 10:
+                self.dragging = True
+                self.button = 0
+                x, y = self.mouse_origin.x, self.mouse_origin.y
+                ctrl.mouse(x, y)
+                ctrl.mouse_click(pos = (x, y), button=0, down=True)
+
+    def on_noise(self, noise):
+        now = time.time()
+        if noise == 'pop':
+            ctrl.mouse_click(button=0, hold=16000)
+
+model = NoiseModel()
