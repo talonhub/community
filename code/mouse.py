ea4da99a1fa96ebaca3ab78f4f7e84107b05445c
knausj85 <knaus.jeff@gmail.com>
2020-04-03 13:37:04 -0700

    mouse enhancements
    - enabling pop to click with 'control mouse' or stop scrolling are now controlled by settings. see mouse.talon for usage
    - add preliminary support for hiding the cursor on windows

--- code/mouse.py
+++ code/mouse.py
@@ -1,4 +1,4 @@
-from talon import cron, ctrl, ui, Module, Context, actions, noise
+from talon import cron, ctrl, ui, Module, Context, actions, noise, settings, registry
 from talon.engine import engine
 from talon_plugins import speech, eye_mouse, eye_zoom_mouse
 import platform
@@ -16,85 +16,52 @@ scroll_job = None
 gaze_job = None
 cancel_scroll_on_pop = True
 
-def on_pop(active):
-    if gaze_job or scroll_job:
-        stop_scroll()
-    elif not eye_zoom_mouse.zoom_mouse.enabled:
-        ctrl.mouse_click(button=0, hold=16000)
+default_cursor = {
+"AppStarting": "%SystemRoot%\\Cursors\\aero_working.ani",
+"Arrow": "%SystemRoot%\\Cursors\\aero_arrow.cur",
+"Hand": "%SystemRoot%\\Cursors\\aero_link.cur",
+"Help": "%SystemRoot%\\Cursors\\aero_helpsel.cur",
+"No": "%SystemRoot%\\Cursors\\aero_unavail.cur",
+"NWPen": "%SystemRoot%\\Cursors\\aero_pen.cur",
+"Person": "%SystemRoot%\\Cursors\\aero_person.cur",
+"Pin": "%SystemRoot%\\Cursors\\aero_pin.cur",
+"SizeAll": "%SystemRoot%\\Cursors\\aero_move.cur",
+"SizeNESW": "%SystemRoot%\\Cursors\\aero_nesw.cur",
+"SizeNS": "%SystemRoot%\\Cursors\\aero_ns.cur",
+"SizeNWSE": "%SystemRoot%\\Cursors\\aero_nwse.cur",
+"SizeWE": "%SystemRoot%\\Cursors\\aero_ew.cur",
+"UpArrow": "%SystemRoot%\Cursors\\aero_up.cur",
+"Wait": '%SystemRoot%\\Cursors\\aero_busy.ani',
+"Crosshair": "",
+"IBeam":"",
+}
 
-if cancel_scroll_on_pop:      
-    noise.register('pop', on_pop)
-
-def show_cursor_helper(show):
-    """Show/hide the cursor"""
-    ctrl.cursor_visible(show)
-    
-def mouse_scroll(amount):
-    def scroll():
-        global scroll_amount
-        if (scroll_amount >= 0) == (amount >= 0):
-            scroll_amount += amount
-        else:
-            scroll_amount = amount
-        actions.mouse_scroll(y=int(amount))
-
-    return scroll
-
-def scroll_continuous_helper():
-    global scroll_amount
-    #print("scroll_continuous_helper")
-    if scroll_amount and eye_zoom_mouse.zoom_mouse.state == eye_zoom_mouse.STATE_IDLE:
-        actions.mouse_scroll(by_lines=False, y=int(scroll_amount / 10))
-
-def start_scroll():
-    global scroll_job
-    scroll_job = cron.interval("60ms", scroll_continuous_helper)
-    
-def gaze_scroll():
-    #print("gaze_scroll")
-    if eye_zoom_mouse.zoom_mouse.state == eye_zoom_mouse.STATE_IDLE:
-        windows = ui.windows()
-        window = None
-        x, y = ctrl.mouse_pos()
-        for w in windows:
-            if w.rect.contains(x, y):
-                window = w.rect
-                break
-        if window is None:
-            #print("no window found!")
-            return
-            
-        midpoint = window.y + window.height / 2
-        amount = int(((y - midpoint) / (window.height / 10)) ** 3)
-        actions.mouse_scroll(by_lines=False, y=amount)
-    
-    #print(f"gaze_scroll: {midpoint} {window.height} {amount}")
-    
-def stop_scroll():
-    global scroll_amount, scroll_job, gaze_job
-    scroll_amount = 0
-    if scroll_job:
-        cron.cancel(scroll_job)
-        
-    if gaze_job:
-        cron.cancel(gaze_job)
-        
-    scroll_job = None
-    gaze_job = None
-    
-def start_cursor_scrolling():
-    global scroll_job, gaze_job
-    stop_scroll()
-    gaze_job = cron.interval("60ms", gaze_scroll)
-
+#todo figure out why notepad++ still shows the cursor sometimes.
+hidden_cursor = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Resources\HiddenCursor.cur")
 
 mod = Module()
 mod.list('mouse_button',   desc='List of mouse button words to mouse_click index parameter')
+mod.setting('mouse_enable_pop_click', 'str')
+mod.setting('mouse_enable_pop_stops_scroll', 'str')
+
+ctx = Context()
+ctx.settings["user.mouse_enable_pop_click"] = False
+ctx.settings["user.mouse_enable_pop_stops_scroll"] = False
+
+ctx.lists['self.mouse_button'] = {
+     #right click
+     'righty':  '1',
+     'rickle': '1',
+     
+     #left click
+     'chiff': '0',
+}
 
 @mod.capture
 def mouse_index(m) -> int:
     "One mouse button index"
     
+
 @mod.action_class
 class Actions:
     def mouse_show_cursor():
@@ -184,12 +151,98 @@ class Actions:
         """Starts gaze scroll"""
         start_cursor_scrolling()
         
-ctx = Context()
-ctx.lists['self.mouse_button'] = {
-     'righty':  '1',
-     'rickle': '1',
-     'chiff': '0',
-}
+def show_cursor_helper(show):
+    """Show/hide the cursor"""
+    if "Windows-10" in platform.platform(terse=True):
+        import winreg, win32con
+
+        try:
+            Registrykey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Control Panel\Cursors", 0, winreg.KEY_WRITE)
+
+            for value_name, value in default_cursor.items():   
+                if show: 
+                    winreg.SetValueEx(Registrykey, value_name, 0, winreg.REG_EXPAND_SZ, value)
+                else:
+                    winreg.SetValueEx(Registrykey, value_name, 0, winreg.REG_EXPAND_SZ, hidden_cursor)
+                        
+            winreg.CloseKey(Registrykey)
+
+            ctypes.windll.user32.SystemParametersInfoA(win32con.SPI_SETCURSORS, 0, None, 0)
+            
+        except WindowsError:
+            print("Unable to show_cursor({})".format(str(show)))
+    else:
+        ctrl.cursor_visible(show)
+
+def on_pop(active):
+    if (gaze_job or scroll_job):
+        if registry.settings["user.mouse_enable_pop_stops_scroll"][1] == 'true':
+            stop_scroll()
+    elif not eye_zoom_mouse.zoom_mouse.enabled and eye_mouse.mouse.attached_tracker is not None:
+        if registry.settings["user.mouse_enable_pop_click"][1] == 'true':
+            ctrl.mouse_click(button=0, hold=16000)
+
+noise.register('pop', on_pop)
+    
+def mouse_scroll(amount):
+    def scroll():
+        global scroll_amount
+        if (scroll_amount >= 0) == (amount >= 0):
+            scroll_amount += amount
+        else:
+            scroll_amount = amount
+        actions.mouse_scroll(y=int(amount))
+
+    return scroll
+
+def scroll_continuous_helper():
+    global scroll_amount
+    #print("scroll_continuous_helper")
+    if scroll_amount and eye_zoom_mouse.zoom_mouse.state == eye_zoom_mouse.STATE_IDLE:
+        actions.mouse_scroll(by_lines=False, y=int(scroll_amount / 10))
+
+def start_scroll():
+    global scroll_job
+    scroll_job = cron.interval("60ms", scroll_continuous_helper)
+    
+def gaze_scroll():
+    #print("gaze_scroll")
+    if eye_zoom_mouse.zoom_mouse.state == eye_zoom_mouse.STATE_IDLE:
+        windows = ui.windows()
+        window = None
+        x, y = ctrl.mouse_pos()
+        for w in windows:
+            if w.rect.contains(x, y):
+                window = w.rect
+                break
+        if window is None:
+            #print("no window found!")
+            return
+            
+        midpoint = window.y + window.height / 2
+        amount = int(((y - midpoint) / (window.height / 10)) ** 3)
+        actions.mouse_scroll(by_lines=False, y=amount)
+    
+    #print(f"gaze_scroll: {midpoint} {window.height} {amount}")
+    
+def stop_scroll():
+    global scroll_amount, scroll_job, gaze_job
+    scroll_amount = 0
+    if scroll_job:
+        cron.cancel(scroll_job)
+        
+    if gaze_job:
+        cron.cancel(gaze_job)
+        
+    scroll_job = None
+    gaze_job = None
+    
+def start_cursor_scrolling():
+    global scroll_job, gaze_job
+    stop_scroll()
+    gaze_job = cron.interval("60ms", gaze_scroll)
+    
+
 
 @ctx.capture(rule='{self.mouse_button}')
 def mouse_index(m) -> int:
--- misc/mouse.talon
+++ misc/mouse.talon
@@ -1,54 +1,37 @@
-control mouse: 
-	user.mouse_toggle_control_mouse()
-
-zoom mouse: 
-	user.mouse_toggle_zoom_mouse()
+settings():
+	#stop continuous scroll/gaze scroll with a pop
+	user.mouse_enable_pop_stops_scroll = 'true'
+	#enable pop click with 'control mouse' mode
+	user.mouse_enable_pop_click = 'true'
 	
-camera overlay: 
-	eye_mouse.camera_overlay.toggle()
-	
-run calibration: 
-	user.mouse_calibrate()
-	
-<user.mouse_index>: 
-	mouse_click(mouse_index)
-	
+control mouse: user.mouse_toggle_control_mouse()
+zoom mouse: user.mouse_toggle_zoom_mouse()
+camera overlay: eye_mouse.camera_overlay.toggle()
+run calibration: user.mouse_calibrate()	
+<user.mouse_index>: mouse_click(mouse_index)
 <user.modifiers> <user.mouse_index>: 
 	key("{modifiers}:down")
 	mouse_click(mouse_index)
 	key("{modifiers}:up")
-	
 (dubclick | duke): 
 	mouse_click()
 	mouse_click()
-
 (tripclick | triplick): 
 	mouse_click()
 	mouse_click()
 	mouse_click()
-
 wheel down: user.mouse_scroll_down()
 wheel tiny [down]: mouse_scroll(20)
 downer: user.mouse_scroll_down_continuous()
-
 wheel up: user.mouse_scroll_up()
 wheel tiny up: mouse_scroll(-20)
 supper: user.mouse_scroll_up_continuous()
-
 gaze wheel: user.mouse_gaze_scroll()
 wheel stop: user.mouse_scroll_stop()
-
 wheel left: mouse_scroll(0, -40)
 wheel tiny left: mouse_scroll(0, -20)
-
 wheel right: mouse_scroll(0, 40)
 wheel tiny right: mouse_scroll(0, 20)
-
-curse yes: 
-	user.mouse_show_cursor()
-	
-curse no: 
-	user.mouse_hide_cursor()
-
-drag: 
-	user.mouse_drag()
+curse yes: user.mouse_show_cursor()
+curse no: user.mouse_hide_cursor()
+drag: user.mouse_drag()
