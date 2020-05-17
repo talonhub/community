e2c4afa672cb607f0ebb670051eaebb2f2f087f0
jonaskindler93 <jonas@psiori.com>
2020-05-17 13:05:34 +0200

    if instead of elif

--- code/mouse.py
+++ code/mouse.py
@@ -190,7 +190,7 @@ def on_pop(active):
     if (gaze_job or scroll_job):
         if settings.get("user.mouse_enable_pop_stops_scroll") >= 1:
             stop_scroll()
-    elif not eye_zoom_mouse.zoom_mouse.enabled and eye_mouse.mouse.attached_tracker is not None:
+    if not eye_zoom_mouse.zoom_mouse.enabled and eye_mouse.mouse.attached_tracker is not None:
         if settings.get("user.mouse_enable_pop_click") >= 1:
             ctrl.mouse_click(button=0, hold=16000)
 
