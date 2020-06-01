819d80f794ebd88ad8334d5448185c2c7d4235c7
knausj85 <knaus.jeff@gmail.com>
2020-06-01 11:59:45 -0600

    vscode.py: Mac title uses emdash

--- apps/vscode/vscode.py
+++ apps/vscode/vscode.py
@@ -1,21 +1,33 @@
 from talon import Context, actions, ui, Module
+import platform
+platform = platform.platform(terse=True)
+is_mac = "Darwin" in platform
 
-ctx_vscode = Context()
-
-ctx_vscode.matches = r'''
+print(platform)
+ctx = Context()
+ctx.matches = r'''
 app: Code
 app: Code - OSS
 app: Code
 app: Visual Studio Code
 app: Code.exe
 '''
-@ctx_vscode.action_class('win')
+@ctx.action_class('win')
 class win_actions:
     def filename(): 
         title = actions.win.title()
-        result = title.split(" - ")[0]
+        #this doesn't seem to be necessary on VSCode for Mac
+        #if title == "":
+        #    title = ui.active_window().doc
+    
+        if is_mac:
+            result = title.split(" — ")[0]
+        else:
+            result = title.split(" - ")[0]
+
         if "." in result:
             return result
+    
         return ""
 
     def file_ext():
