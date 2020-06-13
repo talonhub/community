40ad60b1c5506d48631999f5431c2a1c0314902c
ma-anwar <ma.rizvi.anwar@gmail.com>
2020-06-13 04:53:37 -0400

    Add port mapping for android studio on linux
    On ubuntu, I found the name of the app to be jetbrains-studio, without which no commands would work.

--- code/jetbrains.py
+++ code/jetbrains.py
@@ -42,6 +42,7 @@ port_mapping = {
     "jetbrains-pycharm-ce": 8658,
     "jetbrains-rider": 8660,
     "jetbrains-rubymine": 8661,
+    "jetbrains-studio": 8652,
     "jetbrains-webstorm": 8663,
     "google-android-studio": 8652,
 
