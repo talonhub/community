9d38fbd3695b2b726561a1484fae08e5e64aa660
jcaw <toastedjcaw@gmail.com>
2020-09-30 13:58:46 +0100

    Add file_ext action for jetbrains

--- apps/jetbrains/jetbrains.py
+++ apps/jetbrains/jetbrains.py
@@ -145,6 +145,12 @@ app: jetbrains
 """
 
 
+@ctx.action_class("win")
+class win_actions:
+    def file_ext():
+        return actions.win.title().split(".")[-1]
+
+
 @ctx.action_class("edit")
 class edit_actions:
     def jump_line(n: int):
