94e4d36f939f0daefe8dcbf0562bcce405a463fb
Aaron Adams <aaron.adams@nccgroup.com>
2020-10-07 16:33:08 +0800

    fix comments

--- code/code.py
+++ code/code.py
@@ -113,7 +113,7 @@ class Actions:
         global forced_language
         actions.user.code_clear_language_mode()
         actions.mode.enable("user.{}".format(language))
-        app.notify("Enabled {} mode".format(language))
+        # app.notify("Enabled {} mode".format(language))
         forced_language = True
 
     def code_clear_language_mode():
@@ -123,7 +123,7 @@ class Actions:
 
         for __, lang in extension_lang_map.items():
             actions.mode.disable("user.{}".format(lang))
-        app.notify("Cleared language modes")
+        # app.notify("Cleared language modes")
 
     def code_operator_indirection():
         """code_operator_indirection"""
