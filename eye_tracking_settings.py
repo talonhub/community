2a1f9fbf3c220986c22ac10a4bef4a34077bbea7
knausj85 <knaus.jeff@gmail.com>
2020-10-23 18:39:41 -0600

    Update eye_tracking_settings.py
    Typo

--- eye_tracking_settings.py
+++ eye_tracking_settings.py
@@ -4,6 +4,6 @@
 # if app.platform == 'mac':
 #     eye_zoom_mouse.config.screen_area = Point2d(100, 75)
 #     eye_zoom_mouse.config.img_scale = 6
-# elif app.platformh == 'win':
+# elif app.platform == 'win':
 #     eye_zoom_mouse.config.screen_area = Point2d(200, 150)
 #     eye_zoom_mouse.config.img_scale = 4.5
