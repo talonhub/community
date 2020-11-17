5def25d974d8a16cb712b932b7d22c50408054cb
Michael Arntzenius <daekharel@gmail.com>
2020-11-17 00:29:59 +0000

    vocabulary.py: allow punctuation in text using user.punctuation list

--- code/vocabulary.py
+++ code/vocabulary.py
@@ -1,9 +1,75 @@
 from talon import Context, Module, actions, grammar
 from .user_settings import bind_list_to_csv, bind_word_map_to_csv
 
+mod = Module()
+ctx = Context()
+
+mod.list("vocabulary", desc="additional vocabulary words")
+mod.list("punctuation", desc="words for inserting punctuation into text")
+
+
+@mod.capture(rule="{user.vocabulary}")
+def vocabulary(m) -> str:
+    return m.vocabulary
+
+
+@mod.capture(rule="({user.vocabulary} | <word>)")
+def word(m) -> str:
+    try:
+        return m.vocabulary
+    except AttributeError:
+        # TODO: if the word is both a regular word AND user.vocabulary, then in
+        # principle it may parse as <word> instead; we ought to pass it through
+        # mapping_vocabulary to be sure. But we should be doing that in
+        # user.text, below, too.
+        words = actions.dictate.replace_words(actions.dictate.parse_words(m.word))
+        assert len(words) == 1
+        return words[0]
+
+
+punctuation = set(".,-!?;:")
+
+
+@mod.capture(rule="({user.vocabulary} | {user.punctuation} | <phrase>)+")
+def text(m) -> str:
+    words = []
+    for item in m:
+        if isinstance(item, grammar.vm.Phrase):
+            words.extend(
+                actions.dictate.replace_words(actions.dictate.parse_words(item))
+            )
+        else:
+            words.extend(item.split(" "))
+
+    result = ""
+    for i, word in enumerate(words):
+        if i > 0 and word not in punctuation and words[i - 1][-1] not in ("/-("):
+            result += " "
+        result += word
+    return result
+
+
+
+# ---------- LISTS ----------
+# Default punctuation words.
+default_punctuation = {
+    "period": ".",
+    "comma": ",",
+    "colon": ":",
+    "semicolon": ";", "semi colon": ";",
+    "question mark": "?",
+    "exclamation mark": "!",
+}
+
+bind_list_to_csv(
+    "user.punctuation",
+    "punctuation.csv",
+    csv_headers=("Punctuation Mark", "Word or Phrase"),
+    default_values=default_punctuation,
+)
 
 # Default words that will need to be capitalized (particularly under w2l).
-_capitalize_defaults = [
+capitalize = [
     "I",
     "I'm",
     "I've",
@@ -38,86 +104,34 @@ _capitalize_defaults = [
 ]
 
 # Default words that need to be remapped.
-_word_map_defaults = {
+default_word_map = {
     # E.g:
     # "cash": "cache",
+    # "centre": "center",
 }
+default_word_map.update({word.lower(): word for word in capitalize})
 
-# Default words that should be added to Talon's vocabulary.
-_simple_vocab_default = ["nmap", "admin", "Cisco", "Citrix", "VPN", "DNS", "Minecraft"]
-
-# Defaults for different pronounciations of words that need to be added to
-# Talon's vocabulary.
-_mapping_vocab_default = {
-    "N map": "nmap",
-    "under documented": "under-documented",
-}
-
-
-mod = Module()
-
-
-@mod.capture(rule="{user.vocabulary}")
-def vocabulary(m) -> str:
-    return m.vocabulary
-
-
-@mod.capture(rule="(<user.vocabulary> | <word>)")
-def word(m) -> str:
-    try:
-        return m.vocabulary
-    except AttributeError:
-        # TODO: if the word is both a regular word AND user.vocabulary, then in
-        # principle it may parse as <word> instead; we ought to pass it through
-        # mapping_vocabulary to be sure. But we should be doing that in
-        # user.text, below, too.
-        words = actions.dictate.replace_words(actions.dictate.parse_words(m.word))
-        assert len(words) == 1
-        return words[0]
-
-
-punctuation = set(".,-!?;:")
-
-
-@mod.capture(rule="(<user.vocabulary> | <phrase>)+")
-def text(m) -> str:
-    words = []
-    for item in m:
-        if isinstance(item, grammar.vm.Phrase):
-            words.extend(
-                actions.dictate.replace_words(actions.dictate.parse_words(item))
-            )
-        else:
-            words.extend(item.split(" "))
-
-    result = ""
-    for i, word in enumerate(words):
-        if i > 0 and word not in punctuation and words[i - 1][-1] not in ("/-("):
-            result += " "
-        result += word
-    return result
-
-
-mod.list("vocabulary", desc="user vocabulary")
-
-ctx = Context()
-
-
-_default_word_map = {}
-_default_word_map.update(_word_map_defaults)
-_default_word_map.update({word.lower(): word for word in _capitalize_defaults})
 # "dictate.word_map" is used by `actions.dictate.replace_words` to rewrite words
 # Talon recognized. Entries in word_map don't change the priority with which
 # Talon recognizes some words over others.
 bind_word_map_to_csv(
     "words_to_replace.csv",
     csv_headers=("Replacement", "Original"),
-    default_values=_default_word_map,
+    default_values=default_word_map,
 )
 
-_default_vocabulary = {}
-_default_vocabulary.update({word: word for word in _simple_vocab_default})
-_default_vocabulary.update(_mapping_vocab_default)
+
+# Default words that should be added to Talon's vocabulary.
+simple_vocabulary = ["admin", "VPN", "DNS", "USB", "FAQ", "PhD", "Minecraft"]
+
+# Defaults for different pronounciations of words that need to be added to
+# Talon's vocabulary.
+default_vocabulary = {
+    "N map": "nmap",
+    "under documented": "under-documented",
+}
+default_vocabulary.update({word: word for word in simple_vocabulary})
+
 # "user.vocabulary" is used to explicitly add words/phrases that Talon doesn't
 # recognize. Words in user.vocabulary (or other lists and captures) are
 # "command-like" and their recognition is prioritized over ordinary words.
@@ -125,5 +139,5 @@ bind_list_to_csv(
     "user.vocabulary",
     "additional_words.csv",
     csv_headers=("Word(s)", "Spoken Form (If Different)"),
-    default_values=_default_vocabulary,
+    default_values=default_vocabulary,
 )
