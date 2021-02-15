e31ac723e6a0e75f0f313e4ffb9f3c07fab68fa9
knausj85 <knaus.jeff@gmail.com>
2021-02-14 18:19:22 -0700

    maybe fix dragging on OS X, save for Big Sur

--- code/mouse.py
+++ code/mouse.py
@@ -141,6 +141,7 @@ class Actions:
         """(TEMPORARY) Press and hold/release button 0 depending on state for dragging"""
         # todo: fixme temporary fix for drag command
         button_down = len(list(ctrl.mouse_buttons_down())) > 0
+        print(str(ctrl.mouse_buttons_down()))
         if not button_down:
             # print("start drag...")
             ctrl.mouse_click(button=0, down=True)
@@ -349,3 +350,19 @@ def start_cursor_scrolling():
     gaze_job = cron.interval("60ms", gaze_scroll)
     # if eye_zoom_mouse.zoom_mouse.enabled and eye_mouse.mouse.attached_tracker is not None:
     #    eye_zoom_mouse.zoom_mouse.sleep(True)
+
+
+if app.platform == "mac":
+    from talon import tap
+
+    def on_move(e):
+        if not config.control_mouse:
+            buttons = ctrl.mouse_buttons_down()
+            # print(str(ctrl.mouse_buttons_down()))
+            if not e.flags & tap.DRAG and buttons:
+                e.flags |= tap.DRAG
+                # buttons is a set now
+                e.button = list(buttons)[0]
+                e.modify()
+
+    tap.register(tap.MMOVE | tap.HOOK, on_move)
