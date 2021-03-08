b06b83ef3cabea1eb40033bfaf10c811b19b5950
Michael Arntzenius <daekharel@gmail.com>
2021-03-08 00:26:54 +0000

    Make "command" and "option" keys mac-specific (#378)

--- code/keys.py
+++ code/keys.py
@@ -111,15 +111,17 @@ def letters(m) -> str:
 
 
 ctx = Context()
-ctx.lists["self.modifier_key"] = {
+modifier_keys = {
     # If you find 'alt' is often misrecognized, try using 'alter'.
     "alt": "alt",  #'alter': 'alt',
-    "command": "cmd",
     "control": "ctrl",  #'troll':   'ctrl',
-    "option": "alt",
     "shift": "shift",  #'sky':     'shift',
     "super": "super",
 }
+if app.platform  == "mac":
+    modifier_keys["command"] = "cmd"
+    modifier_keys["option"] = "alt"
+ctx.lists["self.modifier_key"] = modifier_keys
 alphabet = dict(zip(default_alphabet, letters_string))
 ctx.lists["self.letter"] = alphabet
 
