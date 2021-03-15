50e76131dfb54d8b1c853150df608dd4ebcb45a0
knausj85 <knaus.jeff@gmail.com>
2021-03-14 18:35:43 -0600

    Initial work for re-working CSV file support.

--- code/vocabulary.py
+++ code/vocabulary.py
@@ -1,9 +1,69 @@
-from talon import Module
-from .user_settings import bind_list_to_csv, bind_word_map_to_csv
+from talon import Module, Context, resource
+from typing import Dict, List, Tuple
+import os
+import csv
+from pathlib import Path
+from typing import Dict, List, Tuple
 
 mod = Module()
+ctx = Context()
 
 mod.list("vocabulary", desc="additional vocabulary words")
+
+
+# NOTE: This method requires this module to be one folder below the top-level
+#   knausj folder.
+SETTINGS_DIR = Path(__file__).parents[1] / "settings"
+
+if not SETTINGS_DIR.is_dir():
+    os.mkdir(SETTINGS_DIR)
+
+def get_list_from_csv(
+    filename: str, headers: Tuple[str, str], default: Dict[str, str] = {}
+):
+    """Retrieves list from CSV"""
+    path = SETTINGS_DIR / filename
+    assert filename.endswith(".csv")
+
+    if not path.is_file():
+        with open(path, "w", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(headers)
+            for key, value in default.items():
+                writer.writerow([key] if key == value else [value, key])
+
+    # Now read via resource to take advantage of talon's
+    # ability to reload this script for us when the resource changes
+    with resource.open(str(path), "r") as f:
+        rows = list(csv.reader(f))
+
+    # print(str(rows))
+    mapping = {}
+    if len(rows) >= 2:
+        actual_headers = rows[0]
+        if not actual_headers == list(headers):
+            print(
+                f'"{filename}": Malformed headers - {actual_headers}.'
+                + f" Should be {list(headers)}. Ignoring row."
+            )
+        for row in rows[1:]:
+            if len(row) == 0:
+                # Windows newlines are sometimes read as empty rows. :champagne:
+                continue
+            if len(row) == 1:
+                output = spoken_form = row[0]
+            else:
+                output, spoken_form = row[:2]
+                if len(row) > 2:
+                    print(
+                        f'"{filename}": More than two values in row: {row}.'
+                        + " Ignoring the extras."
+                    )
+            # Leading/trailing whitespace in spoken form can prevent recognition.
+            spoken_form = spoken_form.strip()
+            mapping[spoken_form] = output
+
+    return mapping
 
 # Default words that will need to be capitalized (particularly under w2l).
 # NB. These defaults and those later in this file are ONLY used when
@@ -51,13 +111,15 @@ _word_map_defaults = {
 }
 _word_map_defaults.update({word.lower(): word for word in _capitalize_defaults})
 
+
 # "dictate.word_map" is used by `actions.dictate.replace_words` to rewrite words
 # Talon recognized. Entries in word_map don't change the priority with which
 # Talon recognizes some words over others.
-bind_word_map_to_csv(
+
+ctx.settings["dictate.word_map"] = get_list_from_csv(
     "words_to_replace.csv",
-    csv_headers=("Replacement", "Original"),
-    default_values=_word_map_defaults,
+    headers=("Replacement", "Original"),
+    default=_word_map_defaults,
 )
 
 
@@ -75,9 +137,8 @@ _default_vocabulary.update({word: word for word in _simple_vocab_default})
 # "user.vocabulary" is used to explicitly add words/phrases that Talon doesn't
 # recognize. Words in user.vocabulary (or other lists and captures) are
 # "command-like" and their recognition is prioritized over ordinary words.
-bind_list_to_csv(
-    "user.vocabulary",
+ctx.lists["user.vocabulary"] = get_list_from_csv(
     "additional_words.csv",
-    csv_headers=("Word(s)", "Spoken Form (If Different)"),
-    default_values=_default_vocabulary,
+    headers=("Word(s)", "Spoken Form (If Different)"),
+    default=_default_vocabulary,
 )
