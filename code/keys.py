ad0762412e5360825019b853cdb8883adc1faaf9
Alex <alexrkoch@gmail.com>
2021-03-20 16:03:43 -0400

    restore alphabet

--- code/keys.py
+++ code/keys.py
@@ -3,8 +3,8 @@ from typing import Set
 from talon import Module, Context, actions, app
 import sys
 
-default_alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split(
-    " "
+default_alphabet = "air bat char drum each fine gust harp ice jinks crunch lamp made near odd pit quench red sit trap urge vest whale plex yank zip".split(
+   " "
 )
 letters_string = "abcdefghijklmnopqrstuvwxyz"
 
