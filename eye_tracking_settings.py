86ac336c45cc23a8589b1fee3d30db8fe447ce55
Alex <alexrkoch@gmail.com>
2021-04-07 08:47:33 -0400

    changed eye tracking settings

--- eye_tracking_settings.py
+++ eye_tracking_settings.py
@@ -3,8 +3,8 @@ from talon.track.geom import Point2d
 from talon_plugins import speech, eye_mouse, eye_zoom_mouse
 
 if app.platform == "mac":
-    eye_zoom_mouse.config.screen_area = Point2d(130, 130)
-    eye_zoom_mouse.config.img_scale = 5
+    eye_zoom_mouse.config.screen_area = Point2d(200, 125)
+    eye_zoom_mouse.config.img_scale = 3.5
 elif app.platform == "windows":
     eye_zoom_mouse.config.screen_area = Point2d(200, 150)
     eye_zoom_mouse.config.img_scale = 4.5
