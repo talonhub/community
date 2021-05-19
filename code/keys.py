b4a3bc0f1755fddc6874ae792232e7ca86408f63
Chris McRae <codemonkey555@gmail.com>
2021-05-19 00:13:01 -0700

    Tweaks.

--- code/keys.py
+++ code/keys.py
@@ -3,7 +3,7 @@ from typing import Set
 from talon import Module, Context, actions, app
 import sys
 
-default_alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split(
+default_alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yell zip".split(
     " "
 )
 letters_string = "abcdefghijklmnopqrstuvwxyz"
