3d8b8b1b698d2877192d0078d71ca0c28f1f9d17
Benjamin Rutt <ben.rutt@gmail.com>
2021-05-31 16:50:16 -0400

    Enhance function docstrings as per @linuxbochs suggestion

--- misc/datetimeinsert.py
+++ misc/datetimeinsert.py
@@ -8,18 +8,16 @@ ctx = Context()
 @mod.action_class
 class Actions:
     def time_format(fmt: str=None) -> str:
-        """Return current local date or datetime formatted as a str using 'fmt', which
-should be a .strftime() style format string; if 'fmt' is not
-specified, iso format is used"""
+        """Return the current time, formatted.
+        fmt: strftime()-style format string, defaults to ISO format."""
         now = datetime.datetime.now()
         if fmt is None:
             return now.isoformat()
         return now.strftime(fmt)
 
     def time_format_utc(fmt: str=None) -> str:
-        """Return current UTC date or datetime formatted as a str using 'fmt', which
-should be a .strftime() style format string; if 'fmt' is not
-specified, iso format is used"""
+        """Return the current UTC time, formatted.
+        fmt: strftime()-style format string, defaults to ISO format."""
         now = datetime.datetime.utcnow()
         if fmt is None:
             return now.isoformat()
