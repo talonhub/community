03155a88e4f3c504b439430bdc132b9c937b38f2
Stefan du Fresne <stefan@sdufresne.info>
2021-08-03 12:02:29 +0100

    Add unsupported message

--- code/platforms/desktops.py
+++ code/platforms/desktops.py
@@ -1,4 +1,4 @@
-from talon import Module
+from talon import app, Module
 
 mod = Module()
 
@@ -6,21 +6,28 @@ mod = Module()
 class Actions:
     def desktop(number: int):
         """change the current desktop"""
+        app.notify("Not supported on this operating system")
 
     def desktop_show():
         """shows the current desktops"""
+        app.notify("Not supported on this operating system")
 
     def desktop_next():
         """move to next desktop"""
+        app.notify("Not supported on this operating system")
 
     def desktop_last():
         """move to previous desktop"""
+        app.notify("Not supported on this operating system")
 
     def window_move_desktop_left():
         """move the current window to the desktop to the left"""
+        app.notify("Not supported on this operating system")
 
     def window_move_desktop_right():
         """move the current window to the desktop to the right"""
+        app.notify("Not supported on this operating system")
 
     def window_move_desktop(desktop_number: int):
         """move the current window to a different desktop"""
+        app.notify("Not supported on this operating system")
