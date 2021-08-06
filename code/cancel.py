e88cfd4fa5100af36e515ea72dfb0e421baf728c
Michael Arntzenius <daekharel@gmail.com>
2021-08-06 18:41:29 +0100

    implement cancel command properly (#536)

---
+++ code/cancel.py
@@ -0,0 +1,16 @@
+# to disable command cancellation, comment out this entire file.
+# you may also wish to adjust the commands in misc/cancel.talon.
+from talon import speech_system, actions
+
+# To change the phrase used to cancel commands, you must also adjust misc/cancel.talon
+cancel_phrase = "cancel cancel".split()
+
+def pre_phrase(d):
+    n = len(cancel_phrase)
+    before, after = d["text"][:-n], d['text'][-n:]
+    if after != cancel_phrase: return
+    # cancel the command
+    d["parsed"]._sequence = []
+    actions.app.notify(f"Command canceled: {' '.join(before)!r}")
+
+speech_system.register("pre:phrase", pre_phrase)
