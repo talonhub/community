631fe4fc16fd3febd1007f5ffbbb5e1316baf679
DonnerWolfBach <seby.stack@s-sch.net>
2021-08-09 11:14:52 +0200

    removed linux_terminal.py and moved contents to specific terminals

--- apps/platforms/linux/kde-konsole/kde-konsole.py
+++ apps/platforms/linux/kde-konsole/kde-konsole.py
@@ -38,3 +38,18 @@ class app_actions:
     def window_open():
         actions.key('ctrl-shift-n')
 
+# this overwrites the unfitting parts of linux/edit.py
+@ctx.action_class('edit')
+class EditActions:
+    def page_down():
+        actions.key('shift-pagedown')
+    def page_up():
+        actions.key('shift-pageup')
+    def paste():
+        actions.key('ctrl-shift-v')
+    def copy():
+        actions.key('ctrl-shift-c')
+    def find(text: str):
+        actions.key('ctrl-shift-f')
+        if str:
+            actions.insert(text)
