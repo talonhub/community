26fce391daeeabdd27b7aa595ca9cdc63dc084b9
codecat555 <codemonkey555@gmail.com>
2021-09-10 05:20:35 -0700

    Fix KeyError bug. (#575)

--- code/cancel.py
+++ code/cancel.py
@@ -7,10 +7,11 @@ cancel_phrase = "cancel cancel".split()
 
 def pre_phrase(d):
     n = len(cancel_phrase)
-    before, after = d["text"][:-n], d['text'][-n:]
-    if after != cancel_phrase: return
-    # cancel the command
-    d["parsed"]._sequence = []
-    actions.app.notify(f"Command canceled: {' '.join(before)!r}")
+    if 'text' in d and 'parsed' in d:
+        before, after = d["text"][:-n], d['text'][-n:]
+        if after != cancel_phrase: return
+        # cancel the command
+        d["parsed"]._sequence = []
+        actions.app.notify(f"Command canceled: {' '.join(before)!r}")
 
 speech_system.register("pre:phrase", pre_phrase)
