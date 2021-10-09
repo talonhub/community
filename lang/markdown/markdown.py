6ff134771c4852a1d83f8d4fe363293161fb345e
Michael Arntzenius <daekharel@gmail.com>
2021-10-09 15:48:28 +0200

    Can just bind the list globally.

--- lang/markdown/markdown.py
+++ lang/markdown/markdown.py
@@ -2,11 +2,6 @@ from talon import Module, Context
 
 mod = Module()
 ctx = Context()
-ctx.matches = r"""
-mode: user.markdown
-mode: command 
-and code.language: markdown
-"""
 
 mod.list("markdown_code_block_language", desc="Languages for code blocks")
 ctx.lists["user.markdown_code_block_language"] = {
