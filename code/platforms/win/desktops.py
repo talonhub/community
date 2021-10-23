a38042794c899b6967538fdbec470b801991de33
Stefan du Fresne <stefan@sdufresne.info>
2021-10-23 15:25:57 +0100

    Workspace switching support (#513)
    * Workspace switching support
    
    Basic support for moving between workspaces
    
    * Moving to duplicate desktop phrasing
    
    * Missed show
    
    * Add unsupported message
    
    * Update misc/desktops.talon
    
    * Move desktops.py
    
    Co-authored-by: Michael Arntzenius <daekharel@gmail.com>
    Co-authored-by: Pokey Rule <pokey.rule@gmail.com>

---
+++ code/platforms/win/desktops.py
@@ -0,0 +1,24 @@
+from talon import Context, Module, actions
+ctx = Context()
+ctx.matches = r"""
+os: windows
+"""
+
+@ctx.action_class("user")
+class Actions:
+    # def desktop(number: int):
+
+    def desktop_next():
+        actions.key("super-ctrl-right")
+
+    def desktop_last():
+        actions.key("super-ctrl-left")
+
+    def desktop_show():
+        actions.key("super-tab")
+
+    # def window_move_desktop_left():
+
+    # def window_move_desktop_right():
+
+    # def window_move_desktop(desktop_number: int):
