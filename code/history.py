0e6dabcfa080dda8d8f071a8aa3084eb9dae7932
Pokey Rule <755842+pokey@users.noreply.github.com>
2022-02-13 14:08:25 +0000

    PR feedback

--- code/history.py
+++ code/history.py
@@ -18,7 +18,9 @@ def on_phrase(j):
 
     words = j.get('text')
 
-    if text := actions.user.history_transform_phrase_text(words):
+    text = actions.user.history_transform_phrase_text(words)
+
+    if text is not None:
         history.append(text)
         history = history[-setting_command_history_size.get() :]
 
