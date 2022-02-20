7a75d0595f4e963352f3c264faca4a7ef22659f9
Michael Arntzenius <daekharel@gmail.com>
2022-02-20 19:58:08 +0000

    note nonreliability of newlines in docstring

--- code/insert_between.py
+++ code/insert_between.py
@@ -5,7 +5,7 @@ mod = Module()
 @mod.action_class
 class module_actions:
     def insert_between(before: str, after: str):
-        """Insert `before + after`, leaving cursor between `before` and `after`."""
+        """Insert `before + after`, leaving cursor between `before` and `after`. Not entirely reliable if `after` contains newlines."""
         actions.insert(before + after)
         for _ in after: actions.edit.left()
 
