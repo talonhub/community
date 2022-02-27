eea13f5bdd69d6c6a1b2500554a07315bc3fe5ee
RyanRuane <r.arias@gmx.co.uk>
2022-02-28 00:49:16 +0100

    Standardized use of ' over ".

--- apps/platforms/linux/terminator/terminator-linux.py
+++ apps/platforms/linux/terminator/terminator-linux.py
@@ -15,42 +15,42 @@ ctx.matches = r"""
 app: terminator
 """
 ctx.tags = [
-    "terminal",
-    "user.terminal",
-    "user.tabs",
-    "user.splits",
-    "user.generic_unix_shell",
-    "user.git",
-    "user.kubectl",
+    'terminal',
+    'user.terminal',
+    'user.tabs',
+    'user.splits',
+    'user.generic_unix_shell',
+    'user.git',
+    'user.kubectl',
 ]
 
 
 # --- Implement actions ---
-@ctx.action_class("user")
+@ctx.action_class('user')
 class user_actions:
     # user.splits
-    def split_window_right(): actions.key("alt-right")
-    def split_window_left(): actions.key("alt-left")
-    def split_window_down(): actions.key("alt-down")
-    def split_window_up(): actions.key("alt-up")
-    def split_window_vertically(): actions.key("shift-ctrl-e")
-    def split_window_horizontally(): actions.key("shift-ctrl-o")
-    def split_flip(): actions.key("super-r")
-    def split_window(): actions.key("shift-ctrl-o")
-    def split_clear(): actions.key("shift-ctrl-r")
-    def split_clear_all(): actions.key("shift-ctrl-g")
-    def split_next(): actions.key("shift-ctrl-n")
-    def split_last(): actions.key("shift-ctrl-p")
+    def split_window_right(): actions.key('alt-right')
+    def split_window_left(): actions.key('alt-left')
+    def split_window_down(): actions.key('alt-down')
+    def split_window_up(): actions.key('alt-up')
+    def split_window_vertically(): actions.key('shift-ctrl-e')
+    def split_window_horizontally(): actions.key('shift-ctrl-o')
+    def split_flip(): actions.key('super-r')
+    def split_window(): actions.key('shift-ctrl-o')
+    def split_clear(): actions.key('shift-ctrl-r')
+    def split_clear_all(): actions.key('shift-ctrl-g')
+    def split_next(): actions.key('shift-ctrl-n')
+    def split_last(): actions.key('shift-ctrl-p')
 
 
-@ctx.action_class("app")
+@ctx.action_class('app')
 class AppActions:
 
     # app.tabs
-    def tab_open(): actions.key("ctrl-shift-t")
-    def tab_previous(): actions.key("ctrl-pageup")
-    def tab_next(): actions.key("ctrl-pagedown")
-    def tab_close(): actions.key("ctrl-shift-w")
+    def tab_open(): actions.key('ctrl-shift-t')
+    def tab_previous(): actions.key('ctrl-pageup')
+    def tab_next(): actions.key('ctrl-pagedown')
+    def tab_close(): actions.key('ctrl-shift-w')
     # global (overwrite linux/app.py)
     def window_open(): actions.key('ctrl-shift-i')
     def window_close(): actions.key('ctrl-shift-q')
