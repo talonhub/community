2646d6e142b351ede2fb7b6899f6feecf8921dec
David Ray <raydavid@microsoft.com>
2022-03-17 17:38:11 +0000

    stop zoom mouse

--- code/mouse.py
+++ code/mouse.py
@@ -121,8 +121,8 @@ class Actions:
 
     def mouse_wake():
         """Enable control mouse, zoom mouse, and disables cursor"""
-        eye_zoom_mouse.toggle_zoom_mouse(True)
-        # eye_mouse.control_mouse.enable()
+        # eye_zoom_mouse.toggle_zoom_mouse(True)
+        eye_mouse.control_mouse.enable()
         if setting_mouse_wake_hides_cursor.get() >= 1:
             show_cursor_helper(False)
 
