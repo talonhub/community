17ddad8849eecfc67416a059ec4e618506ae154c
Brock McElroy <28877984+brxck@users.noreply.github.com>
2022-04-16 12:03:29 -0700

    Add default typescript function declaration

--- lang/typescript/typescript.py
+++ lang/typescript/typescript.py
@@ -18,6 +18,16 @@ ctx.lists["user.code_type"] = {
 
 @ctx.action_class("user")
 class UserActions:
+    def code_default_function(text: str):
+        """Inserts function declaration"""
+        result = "function {}".format(
+            actions.user.formatted_text(
+                text, settings.get("user.code_private_function_formatter")
+            )
+        )
+        
+        actions.user.code_insert_function(result, None)
+        
     def code_private_function(text: str):
         """Inserts private function declaration"""
         result = "private function {}".format(
