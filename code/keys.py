63899ee4401c999bdc023c446d037b8b14c0b028
Jonathan Weinrib <jonathanweinrib@MacBook-Pro-2.local>
2022-05-10 13:19:43 +0300

    changed letters b, j, k, and u

--- code/keys.py
+++ code/keys.py
@@ -5,7 +5,7 @@ import sys
 
 # default_alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split(
 #     " "
-default_alphabet = "air bug corn drum each fly glen harp sit jack kale lime made near oak play quill red sun toast urge vest wind flex why zip".split(
+default_alphabet = "air burr corn drum each fly glen harp sit jay key lime made near oak play quill red sun toast ute vest wind flex why zip".split(
     " "
 )
 letters_string = "abcdefghijklmnopqrstuvwxyz"
