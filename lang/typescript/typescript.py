be1efbf059056d563a15cb4e1654c2968aa8231f
Stefan Schneider-Kennedy <code@stefansk.name>
2022-06-05 22:17:19 +1000

    Allow unmodified function in typescript

--- lang/typescript/typescript.py
+++ lang/typescript/typescript.py
@@ -18,6 +18,16 @@ ctx.lists["user.code_type"] = {
 
 @ctx.action_class("user")
 class UserActions:
+    def code_default_function(text: str):
+        """Inserts function declaration without modifiers"""
+        result = "function {}".format(
+            actions.user.formatted_text(
+                text, settings.get("user.code_private_function_formatter")
+            )
+        )
+
+        actions.user.code_insert_function(result, None)
+
     def code_private_function(text: str):
         """Inserts private function declaration"""
         result = "private function {}".format(
@@ -28,16 +38,6 @@ class UserActions:
 
         actions.user.code_insert_function(result, None)
 
-    # def code_private_static_function(text: str):
-    #     """Inserts private static function"""
-    #     result = "private static void {}".format(
-    #         actions.user.formatted_text(
-    #             text, settings.get("user.code_private_function_formatter")
-    #         )
-    #     )
-
-    #     actions.user.code_insert_function(result, None)
-
     def code_protected_function(text: str):
         result = "protected function {}".format(
             actions.user.formatted_text(
@@ -47,15 +47,6 @@ class UserActions:
 
         actions.user.code_insert_function(result, None)
 
-    # def code_protected_static_function(text: str):
-    #     result = "protected static void {}".format(
-    #         actions.user.formatted_text(
-    #             text, settings.get("user.code_protected_function_formatter")
-    #         )
-    #     )
-
-    #     actions.user.code_insert_function(result, None)
-
     def code_public_function(text: str):
         result = "public function {}".format(
             actions.user.formatted_text(
@@ -64,15 +55,6 @@ class UserActions:
         )
 
         actions.user.code_insert_function(result, None)
-
-    # def code_public_static_function(text: str):
-    #     result = "public static void {}".format(
-    #         actions.user.formatted_text(
-    #             text, settings.get("user.code_public_function_formatter")
-    #         )
-    #     )
-
-    #     actions.user.code_insert_function(result, None)
 
     def code_insert_type_annotation(type: str):
         actions.insert(f": {type}")
