ff069d7e74ee2918100c66c3ac0c54ad20bc2b20
Gulshan Singh <gsingh2011@gmail.com>
2022-11-17 00:55:37 -0800

    Add more commands to readline tag

--- core/edit/edit.talon
+++ core/edit/edit.talon
@@ -135,13 +135,9 @@ copy word:
     edit.select_word()
     edit.copy()
 
-copy word left:
-    edit.extend_word_left()
-    edit.copy()
+copy word left: user.copy_word_left()
 
-copy word right:
-    edit.extend_word_right()
-    edit.copy()
+copy word right: user.copy_word_right()
 
 copy line:
     edit.select_line()
@@ -169,17 +165,11 @@ cut word:
     edit.select_word()
     edit.cut()
 
-cut word left:
-    edit.extend_word_left()
-    edit.cut()
+cut word left: user.cut_word_left()
 
-cut word right:
-    edit.extend_word_right()
-    edit.cut()
+cut word right: user.cut_word_right()
 
-cut line:
-    edit.select_line()
-    edit.cut()
+cut line: user.cut_line()
 
 (pace | paste) all:
     edit.select_all()
--- tags/terminal/readline.py
+++ tags/terminal/readline.py
@@ -7,7 +7,61 @@ tag: user.readline
 
 
 @ctx.action_class("edit")
-class Actions:
+class EditActions:
     def delete_line():
-        actions.key("ctrl-e")
+        actions.edit.line_end()
         actions.key("ctrl-u")
+
+    def word_left():
+        actions.key("alt-b")
+
+    def word_right():
+        actions.key("alt-f")
+
+    def line_end():
+        actions.key("ctrl-e")
+
+    def line_start():
+        actions.key("ctrl-a")
+
+    def undo():
+        actions.key("ctrl-_")
+
+    # TODO: we don't want to overwrite the system's paste action, should this be a separate command?
+    # def paste():
+    #     actions.key("ctrl-y")
+
+@ctx.action_class("user")
+class Actions:
+    def cut_line():
+        actions.edit.line_start()
+        actions.key("ctrl-k")
+
+    def cut_word_left():
+        actions.key("ctrl-w")
+
+    def cut_word_right():
+        actions.key("alt-d")
+
+    def copy_word_left():
+        actions.user.cut_word_left()
+        actions.key("ctrl-y")
+
+    def copy_word_right():
+        # TODO: how put cursor back at starting position?
+        actions.user.cut_word_right()
+        actions.key("ctrl-y")
+
+    # TODO: not sure how to implement these
+    # def cut_word():
+    #     pass
+    # def copy_word():
+    #     pass
+    # def delete_word():
+    #     pass
+
+    # TODO: not sure how to implement these without overwriting clipboard
+    # def delete_word_left():
+    #     actions.user.cut_word_left()
+    # def delete_word_right():
+    #     actions.user.cut_word_right()
