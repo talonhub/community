5cf1f54cf02209c6cc7c145f10875c43a41ac666
Gulshan Singh <gsingh2011@gmail.com>
2022-11-20 02:49:50 -0800

    Implement 'in' and 'not in' operators for python (#1045)

--- lang/python/python.py
+++ lang/python/python.py
@@ -214,6 +214,12 @@ class UserActions:
     def code_operator_or():
         actions.auto_insert(" or ")
 
+    def code_operator_in():
+        actions.auto_insert(" in ")
+
+    def code_operator_not_in():
+        actions.auto_insert(" not in ")
+
     def code_operator_bitwise_and():
         actions.auto_insert(" & ")
 
