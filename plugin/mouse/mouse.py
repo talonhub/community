b2aabf21614490d5326d1dd18bbc0df94939953b
Ryan Hileman <lunixbochs@gmail.com>
2023-01-15 22:25:15 -0800

    remove the mac mouse event tap

--- plugin/mouse/mouse.py
+++ plugin/mouse/mouse.py
@@ -350,19 +350,3 @@ def start_cursor_scrolling():
     gaze_job = cron.interval("60ms", gaze_scroll)
     # if eye_zoom_mouse.zoom_mouse.enabled and eye_mouse.mouse.attached_tracker is not None:
     #    eye_zoom_mouse.zoom_mouse.sleep(True)
-
-
-if app.platform == "mac":
-    from talon import tap
-
-    def on_move(e):
-        if not config.control_mouse:
-            buttons = ctrl.mouse_buttons_down()
-            # print(str(ctrl.mouse_buttons_down()))
-            if not e.flags & tap.DRAG and buttons:
-                e.flags |= tap.DRAG
-                # buttons is a set now
-                e.button = list(buttons)[0]
-                e.modify()
-
-    tap.register(tap.MMOVE | tap.HOOK, on_move)
