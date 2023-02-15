ff795b203368e628864584b4784a789064e40d2c
David Tejada <davidxtejada@gmail.com>
2023-02-15 17:24:31 +0100

    Remove unnecessary code

--- apps/opera/opera.py
+++ apps/opera/opera.py
@@ -17,21 +17,3 @@ apps.opera = """
 os: linux
 and app.exe: opera
 """
-
-
-ctx.matches = r"""
-app: opera
-"""
-
-
-@ctx.action_class("user")
-class UserActions:
-    def tab_close_wrapper():
-        actions.sleep("180ms")
-        actions.app.tab_close()
-
-
-@ctx.action_class("browser")
-class BrowserActions:
-    def reload_hard():
-        actions.key("shift-f5")
