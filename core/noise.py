3b1a50078c2960adfb92e898508ad848b54d3efe
Stefan Schneider-Kennedy <code@stefansk.name>
2023-03-12 15:37:47 +1100

    feat(#755): Refactor pop to trigger action

---
+++ core/noise.py
@@ -0,0 +1,21 @@
+"""
+Map noises (like pop) to actions so they can have contextually differing behavior
+"""
+
+from talon import Module, noise, actions
+
+
+mod = Module()
+
+
+@mod.action_class
+class Actions:
+    def noise_trigger_pop():
+        """
+        Called when the user makes a 'pop' noise. Listen to
+        https://noise.talonvoice.com/static/previews/pop.mp3 for an
+        example.
+        """
+
+
+noise.register("pop", lambda _: actions.user.noise_trigger_pop())
--- plugin/mouse/mouse.py
+++ plugin/mouse/mouse.py
@@ -1,6 +1,6 @@
 import os
 
-from talon import Module, actions, app, clip, cron, ctrl, imgui, noise, ui
+from talon import Module, Context, actions, app, clip, cron, ctrl, imgui, ui
 from talon_plugins import eye_zoom_mouse
 
 key = actions.key
@@ -38,6 +38,8 @@ hidden_cursor = os.path.join(
 )
 
 mod = Module()
+ctx = Context()
+
 mod.list(
     "mouse_button", desc="List of mouse button words to mouse_click index parameter"
 )
@@ -247,7 +249,8 @@ def show_cursor_helper(show):
         ctrl.cursor_visible(show)
 
 
-def on_pop(_active):
+@ctx.action("user.noise_trigger_pop")
+def on_pop():
     # Only want the pop noise to click when we're using an eye tracker
     is_using_eye_tracker = (
         actions.tracking.control_zoom_enabled()
@@ -260,9 +263,6 @@ def on_pop(_active):
     elif is_using_eye_tracker and not actions.tracking.control_zoom_enabled():
         if setting_mouse_enable_pop_click.get() >= 1:
             ctrl.mouse_click(button=0, hold=16000)
-
-
-noise.register("pop", on_pop)
 
 
 def mouse_scroll(amount):
