2e9baa246424c0fe6ccb7347a156474c5707d803
Jeff Knaus <knaus.jeff@gmail.com>
2023-03-16 23:38:17 -0600

    Update windows.py

--- personal/core/operating_system/windows.py
+++ personal/core/operating_system/windows.py
@@ -108,10 +108,86 @@ class UserActionsWin:
         actions.key("alt-tab")
 
     def system_open_directory(path):
-        actions.user.exec(f'explorer.exe "{path}"')
+        #     actions.user.exec(f'explorer.exe "{path}"')
 
-    def system_show_clipboard():
-        actions.key("super-v")
+        # def system_show_clipboard():
+        #     actions.key("super-v")
+
+        # def system_show_portal(phrase: str = None):
+        #     """Opens the default browser for the up operating system and performs the phrase command"""
+        #     # actions.user.switcher_focus(portal_name.get())
+        #     actions.sleep("250ms")
+        #     if phrase:
+        #         actions.user.parse_phrase(phrase or "")
+
+        # def system_show_coder(phrase: str = None):
+        #     """Opens the default browser for the up operating system and performs the phrase command"""
+        #     is_running = actions.user.switcher_focus(coder_name.get())
+        actions.sleep("250ms")
+
+        actions.user.parse_phrase(phrase or "")
+
+    def system_show_email():
+        """Opens the defaul6t browser for the up operating system and performs the phrase command"""
+        actions.key("super-2")
+        # success = actions.user.switcher_focus_window_by_name(
+        #     portal_name.get(), email_web_address.get()  # "https://outlook.office.com/"
+        # )
+        # if not success:
+        #     actions.user.open_new_url(email_web_address.get())
+        # is_running = actions.user.switcher_focus("gmail")
+
+        # is_running = actions.user.switcher_focus("outlook")
+        # actions.sleep("250ms")
+        # if is_running:
+        #     actions.user.parse_phrase(phrase or "")
+
+    def system_show_messenger():
+        """Opens the default browser for the up operating system and performs the phrase command"""
+        # is_running = actions.user.switcher_focus(messaging_application.get())
+        actions.key("super-3")
+        # success = actions.user.switcher_focus_window_by_name(
+        #     portal_name.get(), "https://teams.microsoft.com/"
+        # )
+        # if not success:
+        #     actions.user.open_new_url("https://teams.microsoft.com/")
+
+    def system_show_slacker():
+        """Opens the default browser for the up operating system and performs the phrase command"""
+        actions.key("super-4")
+        # success = actions.user.switcher_focus_window_by_name(
+        #     portal_name.get(), "https://app.slack.com/"
+        # )
+        # if not success:
+        #     actions.user.open_url("https://app.slack.com/")
+        # is_running = actions.user.switcher_focus("slack")
+        # actions.sleep("250ms")
+        # if is_running:
+        #     actions.user.parse_phrase(phrase or "")
+
+    def system_show_gitter(phrase: str = None):
+        """Opens the default browser for the up operating system and performs the phrase command"""
+        actions.key("super-5")
+        # is_running = actions.user.switcher_focus("git hub")
+        # actions.sleep("250ms")
+        # if is_running:
+        #     actions.user.parse_phrase(phrase or "")
+
+    def system_show_portal(phrase: str = None):
+        """Opens the default browser for the up operating system and performs the phrase command"""
+        actions.key("super-6")
+        # actions.user.switcher_focus(portal_name.get())
+        # actions.sleep("250ms")
+        # if phrase:
+        #     actions.user.parse_phrase(phrase or "")
+
+    def system_show_coder(phrase: str = None):
+        """Opens the default browser for the up operating system and performs the phrase command"""
+        actions.key("super-7")
+        # is_running = actions.user.switcher_focus(coder_name.get())
+        # actions.sleep("250ms")
+
+        # actions.user.parse_phrase(phrase or "")
 
     def system_kill_focused_application():
         """Kills the focused application"""
