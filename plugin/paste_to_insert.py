7da9795e2a41f9aad4900a3a814a92eda789428e
Michael Arntzenius <daekharel@gmail.com>
2023-03-19 21:58:10 +0000

    add paste-to-insert plugin & setting

---
+++ plugin/paste_to_insert.py
@@ -0,0 +1,22 @@
+from talon import Module, Context, actions
+import logging
+
+mod = Module()
+ctx = Context()
+
+paste_to_insert_threshold_setting = mod.setting(
+    "paste_to_insert_threshold",
+    type=int,
+    default=-1,
+    desc="""Use paste to insert text longer than this many characters.
+Zero means always paste; -1 means never paste.
+""")
+
+@ctx.action_class("main")
+class MainActions:
+    def insert(text):
+        threshold = paste_to_insert_threshold_setting.get()
+        if 0 <= threshold < len(text):
+            actions.user.paste(text)
+            return
+        actions.next(text)
