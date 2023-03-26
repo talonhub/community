1b310246d98137e336fd39c6911151d9949ab4c9
Jeff Knaus <knaus.jeff@gmail.com>
2023-03-26 08:59:18 -0600

    Update mac.py

--- personal/core/operating_system/mac.py
+++ personal/core/operating_system/mac.py
@@ -138,6 +138,6 @@ ctx.lists["self.system_directories"] = {
     "talent home": os.path.expanduser("~/.talon"),
     "talent user": os.path.expanduser("~/.talon/user"),
     "talent recordings": os.path.expanduser("~/.talon/recordings"),
-    "talent plugins": "/Applications/Talon.app234/Contents/Resources/talon_plugins",
+    "talent plugins": "/Applications/Talon.app/Contents/Resources/talon_plugins",
     "root": "/",
 }
