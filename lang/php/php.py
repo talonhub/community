fda4bca2e6d8d0084d572459156a1083f63f6242
Gabriele <gmthedoc@gmail.com>
2023-03-27 19:34:41 +0200

    Fix code for private static functions

--- lang/php/php.py
+++ lang/php/php.py
@@ -216,7 +216,14 @@ class UserActions:
         actions.edit.left()
 
     def code_private_static_function(text: str):
-        actions.user.code_public_static_function(text)
+        """Inserts private static function declaration"""
+        result = "private static function {}()".format(
+            actions.user.formatted_text(
+                text, settings.get("user.code_protected_function_formatter")
+            )
+        )
+        actions.user.paste(result)
+        actions.edit.left()
 
     def code_protected_static_function(text: str):
         """Inserts protected static function declaration"""
