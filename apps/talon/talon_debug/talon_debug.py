2126560ee51ad25bd65cbb0c32955d41d5640ec6
Jeff Knaus <knaus.jeff@gmail.com>
2023-04-03 17:24:05 -0600

    Update talon_debug.py

--- apps/talon/talon_debug/talon_debug.py
+++ apps/talon/talon_debug/talon_debug.py
@@ -5,4 +5,10 @@ mod.apps.talon_debug = """
 os: mac
 and app.bundle: com.talonvoice.Talon
 win.title: Talon Debug
-""" 
+"""
+mod.apps.talon_debug = """
+os: windows
+and app.name: Talon
+os: windows
+and app.exe: talon.exe   
+"""
