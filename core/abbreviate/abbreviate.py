f2c7edbec3334269ef570c2ec633bb1caa9b6d81
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-05-05 13:18:22 +0200

    Allows the abbreviated/short form to be used as spoken phrase

--- core/abbreviate/abbreviate.py
+++ core/abbreviate/abbreviate.py
@@ -448,5 +448,11 @@ abbreviations_list = get_list_from_csv(
     headers=("Abbreviation", "Spoken Form"),
     default=abbreviations,
 )
+
 ctx = Context()
-ctx.lists["user.abbreviation"] = abbreviations_list
+
+# Allows the abbreviated/short form to be used as spoken phrase
+ctx.lists["user.abbreviation"] = {
+    **abbreviations_list,
+    **{v: v for v in abbreviations_list.values()},
+}
