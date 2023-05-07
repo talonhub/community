9008ce2aba0e39dee74065ce4bd65b1348f5ab1e
Jeff Knaus <knaus.jeff@gmail.com>
2023-05-07 11:30:27 -0600

    tweaks

--- core/help/help_scope.py
+++ core/help/help_scope.py
@@ -1,7 +1,7 @@
 from talon import Module, Context, actions, imgui, scope, ui
 ctx = Context()
 mod = Module()
-mod.tag("help_scope", "tag for showing the scope help gui")
+mod.tag("help_scope_open", "tag for showing the scope help gui")
 
 setting_max_length = mod.setting(
     "help_scope_max_length",
@@ -64,5 +64,5 @@ class Actions:
             ctx.tags = []
             gui.hide()
         else:
-            ctx.tags = ["user.help_scope"]
+            ctx.tags = ["user.help_scope_open"]
             gui.show()
--- core/help/help_scope_open.talon
+++ core/help/help_scope_open.talon
@@ -1,4 +1,4 @@
-tag: user.help_scope
+tag: user.help_scope_open
 -
 
 scope (hide | close)$: user.help_scope_toggle()
