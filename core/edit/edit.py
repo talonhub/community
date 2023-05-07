6de8bfa29fbea906c4a17aff39c466576f82c6f5
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-05-07 11:56:52 +0200

    Added additional edit commands (#1182)
    `"copy start of line"`
    `"clear end of line"`
    
    `"paste line"`
    `"paste word"`
    
    `"drink line"`
    `"pour line"`
    
    Made sure all operations(select, copy, cut, paste, delete) had
    implementations for all, line, line start, line end, and word. Grouped a
    few commands to make finding then easier and added actions for reusable
    logic.
    
    ---------
    
    Co-authored-by: Phil Cohen <phillip@phillip.io>

--- core/edit/edit.py
+++ core/edit/edit.py
@@ -79,6 +79,10 @@ class Actions:
             # sleep here so that clip.revert doesn't revert the clipboard too soon
             actions.sleep("150ms")
 
+    def delete_right():
+        """Delete character to the right"""
+        actions.key("delete")
+
     def words_left(n: int):
         """Moves left by n words."""
         for _ in range(n):
@@ -88,6 +92,11 @@ class Actions:
         """Moves right by n words."""
         for _ in range(n):
             actions.edit.word_right()
+
+    def cut_word():
+        """Cut word under cursor"""
+        actions.edit.select_word()
+        actions.edit.cut()
 
     def cut_word_left():
         """Cuts the word to the left."""
@@ -99,10 +108,10 @@ class Actions:
         actions.edit.extend_word_right()
         actions.edit.cut()
 
-    def cut_line():
-        """Cuts the current line."""
-        actions.edit.select_line()
-        actions.edit.cut()
+    def copy_word():
+        """Copy word under cursor"""
+        actions.edit.select_word()
+        actions.edit.copy()
 
     def copy_word_left():
         """Copies the word to the left."""
@@ -113,3 +122,96 @@ class Actions:
         """Copies the word to the right."""
         actions.edit.extend_word_right()
         actions.edit.copy()
+
+    def paste_word():
+        """Paste to word under cursor"""
+        actions.edit.select_word()
+        actions.edit.paste()
+
+    def cut_all():
+        """Cut all text in the current document"""
+        actions.edit.select_all()
+        actions.edit.cut()
+
+    def copy_all():
+        """Copy all text in the current document"""
+        actions.edit.select_all()
+        actions.edit.copy()
+
+    def paste_all():
+        """Paste to the current document"""
+        actions.edit.select_all()
+        actions.edit.paste()
+
+    def delete_all():
+        """Delete all text in the current document"""
+        actions.edit.select_all()
+        actions.edit.delete()
+
+    def cut_line():
+        """Cut current line"""
+        actions.edit.select_line()
+        actions.edit.cut()
+
+    def copy_line():
+        """Copy current line"""
+        actions.edit.select_line()
+        actions.edit.copy()
+
+    def paste_line():
+        """Paste to current line"""
+        actions.edit.select_line()
+        actions.edit.paste()
+
+    # ----- Start / End of line -----
+    def select_line_start():
+        """Select to start of current line"""
+        if actions.edit.selected_text():
+            actions.edit.left()
+        actions.edit.extend_line_start()
+
+    def select_line_end():
+        """Select to end of current line"""
+        if actions.edit.selected_text():
+            actions.edit.right()
+        actions.edit.extend_line_end()
+
+    def cut_line_start():
+        """Cut to start of current line"""
+        actions.user.select_line_start()
+        actions.edit.cut()
+
+    def cut_line_end():
+        """Cut to end of current line"""
+        actions.user.select_line_end()
+        actions.edit.cut()
+
+    def copy_line_start():
+        """Copy to start of current line"""
+        actions.user.select_line_start()
+        actions.edit.copy()
+
+    def copy_line_end():
+        """Copy to end of current line"""
+        actions.user.select_line_end()
+        actions.edit.copy()
+
+    def paste_line_start():
+        """Paste to start of current line"""
+        actions.user.select_line_start()
+        actions.edit.paste()
+
+    def paste_line_end():
+        """Paste to end of current line"""
+        actions.user.select_line_end()
+        actions.edit.paste()
+
+    def delete_line_start():
+        """Delete to start of current line"""
+        actions.user.select_line_start()
+        actions.edit.delete()
+
+    def delete_line_end():
+        """Delete to end of current line"""
+        actions.user.select_line_end()
+        actions.edit.delete()
--- core/edit/edit.talon
+++ core/edit/edit.talon
@@ -1,79 +1,62 @@
+# Searching
 find it: edit.find()
-
 next one: edit.find_next()
 
 go word left: edit.word_left()
-
 go word right: edit.word_right()
 
 go left: edit.left()
-
 go right: edit.right()
-
 go up: edit.up()
-
 go down: edit.down()
 
 go line start: edit.line_start()
-
 go line end: edit.line_end()
 
 go way left:
     edit.line_start()
     edit.line_start()
-
 go way right: edit.line_end()
-
+go way up: edit.file_start()
 go way down: edit.file_end()
 
-go way up: edit.file_start()
-
+go top: edit.file_start()
 go bottom: edit.file_end()
 
-go top: edit.file_start()
-
+go page up: edit.page_up()
 go page down: edit.page_down()
 
-go page up: edit.page_up()
-
-# selecting
-select line: edit.select_line()
-
+# Selecting
 select all: edit.select_all()
+select line: edit.select_line()
+select line start: user.select_line_start()
+select line end: user.select_line_end()
 
 select left: edit.extend_left()
-
 select right: edit.extend_right()
-
 select up: edit.extend_line_up()
-
 select down: edit.extend_line_down()
 
 select word: edit.select_word()
-
 select word left: edit.extend_word_left()
-
 select word right: edit.extend_word_right()
 
 select way left: edit.extend_line_start()
-
 select way right: edit.extend_line_end()
-
 select way up: edit.extend_file_start()
-
 select way down: edit.extend_file_end()
 
-# editing
+# Indentation
 indent [more]: edit.indent_more()
-
 (indent less | out dent): edit.indent_less()
 
-# deleting
+# Delete
+clear all: user.delete_all()
 clear line: edit.delete_line()
-
-clear left: key(backspace)
-
-clear right: key(delete)
+clear line start: user.delete_line_start()
+clear line end: user.delete_line_end()
+clear left: edit.delete()
+clear right: user.delete_right()
 
 clear up:
     edit.extend_line_up()
@@ -109,14 +92,15 @@ clear way down:
     edit.extend_file_end()
     edit.delete()
 
-clear all:
-    edit.select_all()
-    edit.delete()
+# Copy
+copy all: user.copy_all()
+copy line: user.copy_line()
+copy line start: user.copy_line_start()
+copy line end: user.copy_line_end()
+copy word: user.copy_word()
+copy word left: user.copy_word_left()
+copy word right: user.copy_word_right()
 
-#copy commands
-copy all:
-    edit.select_all()
-    edit.copy()
 #to do: do we want these variants, seem to conflict
 # copy left:
 #      edit.extend_left()
@@ -131,22 +115,15 @@ copy all:
 #     edit.extend_down()
 #     edit.copy()
 
-copy word:
-    edit.select_word()
-    edit.copy()
+# Cut
+cut all: user.cut_all()
+cut line: user.cut_line()
+cut line start: user.cut_line_start()
+cut line end: user.cut_line_end()
+cut word: user.cut_word()
+cut word left: user.cut_word_left()
+cut word right: user.cut_word_right()
 
-copy word left: user.copy_word_left()
-
-copy word right: user.copy_word_right()
-
-copy line:
-    edit.select_line()
-    edit.copy()
-
-#cut commands
-cut all:
-    edit.select_all()
-    edit.cut()
 #to do: do we want these variants
 # cut left:
 #      edit.select_all()
@@ -161,20 +138,17 @@ cut all:
 #     edit.select_all()
 #     edit.cut()
 
-cut word:
-    edit.select_word()
-    edit.cut()
+# Paste
+(pace | paste) all: user.paste_all()
+(pace | paste) line: user.paste_line()
+(pace | paste) line start: user.paste_line_start()
+(pace | paste) line end: user.paste_line_end()
+(pace | paste) word: user.paste_word()
 
-cut word left: user.cut_word_left()
-
-cut word right: user.cut_word_right()
-
-cut line: user.cut_line()
-
-(pace | paste) all:
-    edit.select_all()
-    edit.paste()
-
-# duplication
+# Duplication
 clone that: edit.selection_clone()
 clone line: edit.line_clone()
+
+# Insert new line
+new line above: edit.line_insert_up()
+new line below: edit.line_insert_down()
