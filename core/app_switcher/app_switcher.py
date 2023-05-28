cf9f5ba74ad69f50d90c9ec6a9f28eb27aebb1f3
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2023-05-28 17:53:11 +0100

    new 'focus last' implementation that only works on Windows but that is better

--- core/app_switcher/app_switcher.py
+++ core/app_switcher/app_switcher.py
@@ -25,7 +25,6 @@ overrides = {}
 # a list of the currently running application names
 running_application_dict = {}
 
-last_focused_app = None
 
 mac_application_directories = [
     "/Applications",
@@ -257,16 +256,13 @@ class Actions:
 
     def switch_last_focused():
         """Focus the previously used application"""
-        global last_focused_app
-        if last_focused_app is not None:
-            current_focused_app = ui.active_app()
-            last_focused_app.focus()
-            last_focused_app = current_focused_app
+        if app.platform == "windows":
+            actions.key("alt-tab")
+        else:
+            print("last focused app not supported yet on " + app.platform)
 
     def switcher_focus(name: str):
         """Focus a new application by name"""
-        global last_focused_app
-        last_focused_app = ui.active_app()
         app = actions.user.get_running_app(name)
         actions.user.switcher_focus_app(app)
 
