e5037334ecb60c631960c0b8439422ea51640454
Jeff Knaus <knaus.jeff@gmail.com>
2023-06-10 18:18:57 -0500

    Create work.py

---
+++ work/work.py
@@ -0,0 +1,7 @@
+from talon import Context, Module, actions, ui, app
+import os
+
+mod = Module()
+mod.list("employee_names", desc="list of employee names")
+mod.list("employee_perforce_aliases", desc="list of employee perforce aliases")
+mod.list("employee_full_name", desc="list of employee full names")
