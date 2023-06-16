345ff540e3214f2973c34714362712814ba78b9a
brief <91160+brief@users.noreply.github.com>
2023-06-15 19:23:40 -0700

    Correctly capitalize words using the CAPITALIZE_ALL_WORDS and CAPITALIZE_FIRST_WORD formatters

--- core/text/formatters.py
+++ core/text/formatters.py
@@ -125,10 +125,10 @@ formatters_dict = {
     "DOT_SEPARATED": words_with_joiner("."),
     "DOT_SNAKE": (NOSEP, lambda i, word, _: "." + word if i == 0 else "_" + word),
     "SLASH_SEPARATED": (NOSEP, every_word(lambda w: "/" + w)),
-    "CAPITALIZE_FIRST_WORD": (SEP, first_vs_rest(lambda w: w.capitalize())),
+    "CAPITALIZE_FIRST_WORD": (SEP, first_vs_rest(lambda w: w[:1].upper() + w[1:])),
     "CAPITALIZE_ALL_WORDS": (
         SEP,
-        lambda i, word, _: word.capitalize()
+        lambda i, word, _: word[:1].upper() + word[1:]
         if i == 0 or word not in words_to_keep_lowercase
         else word,
     ),
