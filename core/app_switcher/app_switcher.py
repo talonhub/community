talonhub/community

commit 609ed7a701e93c39d6e546e71f891385fd9d267b

add support for launching Linux apps (#1197)
Added ability to launch Linux apps. Parses .desktop shortcut files for
app launch commands. Correctly parses arguments and passes them to
ui.launch.

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

core/app_switcher/app_switcher.py
@@ -1,4 +1,5 @@
 import os
+import shlex
 import subprocess
 import time
 from pathlib import Path
@@ -31,6 +32,14 @@ mac_application_directories = [
     "/Applications/Utilities",
     "/System/Applications",
     "/System/Applications/Utilities",
+]
+
+linux_application_directories = [
+    "/usr/share/applications",
+    "/usr/local/share/applications",
+    os.path.expandvars("/home/$USER/.local/share/applications"),
+    "/var/lib/flatpak/exports/share/applications",
+    "/var/lib/snapd/desktop/applications",
 ]
 
 words_to_exclude = [
@@ -162,6 +171,39 @@ if app.platform == "windows":
         return items
 
 
+if app.platform == "linux":
+    import configparser
+    import re
+
+    def get_linux_apps():
+        # app shortcuts in program menu are contained in .desktop files. This function parses those files for the app name and command
+        items = {}
+        # find field codes in exec key with regex
+        # https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html#exec-variables
+        args_pattern = re.compile(r" \%[UufFcik]")
+        for base in linux_application_directories:
+            if os.path.isdir(base):
+                for entry in os.scandir(base):
+                    if entry.name.endswith(".desktop"):
+                        config = configparser.ConfigParser(interpolation=None)
+                        config.read(entry.path)
+                        # only parse shortcuts that are not hidden
+                        if config.has_option("Desktop Entry", "NoDisplay") == False:
+                            name_key = config["Desktop Entry"]["Name"]
+                            exec_key = config["Desktop Entry"]["Exec"]
+                            # remove extra quotes from exec
+                            if exec_key[0] == '"' and exec_key[-1] == '"':
+                                exec_key = re.sub('"', "", exec_key)
+                            # remove field codes and add full path if necessary
+                            if exec_key[0] == "/":
+                                items[name_key] = re.sub(args_pattern, "", exec_key)
+                            else:
+                                items[name_key] = "/usr/bin/" + re.sub(
+                                    args_pattern, "", exec_key
+                                )
+        return items
+
+
 @mod.capture(rule="{self.running}")  # | <user.text>)")
 def running_applications(m) -> str:
     "Returns a single application name"
@@ -280,7 +322,10 @@ class Actions:
     def switcher_launch(path: str):
         """Launch a new application by path (all OSes), or AppUserModel_ID path on Windows"""
         if app.platform != "windows":
-            ui.launch(path=path)
+            # separate command and arguments
+            cmd = shlex.split(path)[0]
+            args = shlex.split(path)[1:]
+            ui.launch(path=cmd, args=args)
         else:
             is_valid_path = False
             try:
@@ -337,6 +382,10 @@ def update_launch_list():
 
     elif app.platform == "windows":
         launch = get_windows_apps()
+
+    elif app.platform == "linux":
+        launch = get_linux_apps()
+
         # actions.user.talon_pretty_print(launch)
 
     ctx.lists["self.launch"] = actions.user.create_spoken_forms_from_map(

commit 207c6a132a885fbd82fd513a993626b6af9b4a25

fix: Handle spaces for 'launch <appname>' in OSX
Regression from https://github.com/talonhub/community/pull/1197

core/app_switcher/app_switcher.py
@@ -384,7 +384,7 @@ def update_launch_list():
                 for name in os.listdir(base):
                     path = os.path.join(base, name)
                     name = name.rsplit(".", 1)[0].lower()
-                    launch[name] = path
+                    launch[name] = f"'{path}'"
 
     elif app.platform == "windows":
         launch = get_windows_apps()