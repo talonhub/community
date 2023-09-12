162b82c09f96236a1fae37b3861a830d94116d6c
Roku <rokubop@gmail.com>
2023-09-11 21:11:52 -0500

    Add optional homophone code for cancel.py

--- plugin/cancel/cancel.py
+++ plugin/cancel/cancel.py
@@ -1,20 +1,27 @@
 # to disable command cancellation, comment out this entire file.
-# you may also wish to adjust the commands in misc/cancel.talon.
+# you may also wish to adjust the commands in plugin/cancel/cancel.talon.
 from talon import actions, speech_system
 
-# To change the phrase used to cancel commands, you must also adjust misc/cancel.talon
-cancel_phrase = "cancel cancel".split()
+# To change the phrase used to cancel commands, you must also adjust plugin/cancel/cancel.talon
+cancel_phrases = ["cancel cancel"]
 
+# Alternatively, if you want to use a two-word phrase that
+# can include homophones, you can use this code instead:
+# cancel_words = ["cancel", "counsel", "council"]
+# cancel_phrases = [
+#     " ".join([word1, word2])
+#     for word1 in cancel_words
+#     for word2 in cancel_words
+# ]
 
 def pre_phrase(d):
-    n = len(cancel_phrase)
     if "text" in d and "parsed" in d:
-        before, after = d["text"][:-n], d["text"][-n:]
-        if after != cancel_phrase:
-            return
-        # cancel the command
-        d["parsed"]._sequence = []
-        actions.app.notify(f"Command canceled: {' '.join(before)!r}")
-
+        text_str = ' '.join(d["text"])
+        for phrase in cancel_phrases:
+            if text_str.endswith(phrase):
+                # cancel the command
+                d["parsed"]._sequence = []
+                actions.app.notify(f"Command canceled: {text_str!r}")
+                return
 
 speech_system.register("pre:phrase", pre_phrase)
