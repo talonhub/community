2cc464a7bb7a5cd5aa6bfbf30e5847e90eca5b03
Jeff Knaus <knaus.jeff@gmail.com>
2023-11-12 10:31:25 -0700

    merge

--- core/snippets/snippets_insert.py
+++ core/snippets/snippets_insert.py
@@ -1,4 +1,5 @@
 import re
+from typing import Any
 
 from talon import Module, actions
 
@@ -47,3 +48,28 @@ class Actions:
             )
 
         actions.user.insert_snippet_by_name(name, substitutions)
+
+    def insert_wrapper_snippet(
+        body: str,
+        target: Any,
+        scope: str = None,
+    ):
+        """Wrap the target with snippet"""
+
+    def insert_wrapper_snippet_by_name(name: str, target: Any):
+        """Wrap the target with snippet <name>"""
+        index = name.rindex(".")
+        snippet_name = name[:index]
+        variable_name = name[index + 1]
+        snippet: Snippet = actions.user.get_snippet(snippet_name)
+        variable = snippet.get_variable_strict(variable_name)
+        reg = re.compile(rf"\${variable_name}|\$\{{{variable_name}\}}")
+
+        if not reg.search(snippet.body):
+            raise ValueError(
+                f"Can't wrap non existing variable '{variable_name}' in snippet body '{snippet.body}'"
+            )
+
+        body = reg.sub("$TM_SELECTED_TEXT", snippet.body)
+
+        actions.user.insert_wrapper_snippet(body, target, variable.wrapper_scope)
