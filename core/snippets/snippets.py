ce6af39016458fd3fb951b22ac90f3093eb0e614
Jeff Knaus <knaus.jeff@gmail.com>
2023-11-11 22:58:15 -0700

    merge

--- core/snippets/snippets.py
+++ core/snippets/snippets.py
@@ -39,7 +39,14 @@ for lang in language_ids:
 def get_setting_dir():
     if not setting_dir.get():
         return None
-    return Path(actions.path.talon_user()) / setting_dir.get()
+
+    dir = Path(setting_dir.get())
+
+    if not dir.is_absolute():
+        user_dir = Path(actions.path.talon_user())
+        dir = user_dir / dir
+
+    return dir.resolve()
 
 
 @mod.action_class
@@ -174,7 +181,6 @@ def create_lists(
 
 
 def on_ready():
-    print("on_ready")
     fs.watch(str(SNIPPETS_DIR), lambda _1, _2: update_snippets())
 
     if get_setting_dir():
--- core/windows_and_tabs/windows_and_tabs_linux.py
+++ core/windows_and_tabs/windows_and_tabs_linux.py
@@ -35,14 +35,6 @@ class AppActions:
 
     def window_hide_others():
         actions.key("win-d alt-tab")
-        # requires easy window switcher or equivalent (built into most Linux)
-
-    def window_next():
-        actions.key("alt-`")
 
     def window_open():
         actions.key("ctrl-n")
-        # requires easy window switcher or equivalent (built into most Linux)
-
-    def window_previous():
-        actions.key("alt-shift-`")
