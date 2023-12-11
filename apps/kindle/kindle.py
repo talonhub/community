61b2f0484ece85edefdbe177d803ed685a4903e1
Jeff Knaus <knaus.jeff@gmail.com>
2023-12-10 18:43:29 -0700

    cleanup

--- apps/kindle/kindle.py
+++ apps/kindle/kindle.py
@@ -1,4 +1,4 @@
-from talon import Module
+from talon import Context, actions, Module
 
 # --- App definition ---
 mod = Module()
@@ -8,4 +8,46 @@ and app.name: Kindle
 os: windows
 and app.exe: Kindle.exe
 """
-# TODO: mac context and implementation
+mod.apps.kindle = """
+os: mac
+and app.bundle: com.amazon.Lassen
+"""
+
+ctx_win = Context()
+ctx_win.matches = """
+os: windows
+app: kindle
+"""
+
+ctx_mac = Context()
+ctx_mac.matches = """
+os: mac
+app: kindle
+"""
+
+@ctx_win.action_class("user")
+class UserActions:
+    # user.pages
+    def page_next():
+        actions.key("down")
+
+    def page_previous():
+        actions.key("up")
+
+    def page_jump(number: int):
+        actions.key("ctrl-g")
+        actions.insert(str(number))
+        actions.key("enter")
+@ctx_mac.action_class("user")
+class UserActions:
+    # user.pages
+    def page_next():
+        actions.key("down")
+
+    def page_previous():
+        actions.key("up")
+
+    # def page_jump(number: int):
+    #     actions.key("ctrl-g")
+    #     actions.insert(str(number))
+    #     actions.key("enter")
