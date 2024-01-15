d904b4b299bbc86a490cbddca5f82b1c054370f7
brief <91160+brief@users.noreply.github.com>
2024-01-15 07:33:31 -0800

    Use settings.get() in more places (#1351)
    Builds on #1349 by converting most uses of `setting = mod.setting()` in
    community to `settings.get()`.
    
    @lunixbochs, would appreciate a quick look to make sure this is
    correctly using settings.
    
    ---------
    
    Co-authored-by: Nicholas Riley <com-github@sabi.net>

--- core/deprecations.py
+++ core/deprecations.py
@@ -53,12 +53,12 @@ import datetime
 import os.path
 import warnings
 
-from talon import Module, actions, speech_system
+from talon import Module, actions, settings, speech_system
 
 REPO_DIR = os.path.dirname(os.path.dirname(__file__))
 
 mod = Module()
-setting_deprecate_warning_interval_hours = mod.setting(
+mod.setting(
     "deprecate_warning_interval_hours",
     type=float,
     desc="""How long, in hours, to wait before notifying the user again of a
@@ -98,7 +98,7 @@ def deprecate_notify(id: str, message: str):
 
     maybe_last_shown = notification_last_shown.get(id)
     now = datetime.datetime.now()
-    interval = setting_deprecate_warning_interval_hours.get()
+    interval = settings.get("user.deprecate_warning_interval_hours")
     threshold = now - datetime.timedelta(hours=interval)
     if maybe_last_shown is not None and maybe_last_shown > threshold:
         return
