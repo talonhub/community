34b6c4577d3dd16130cb941976a1c677ca62fc00
Jeff Knaus <knaus.jeff@gmail.com>
2024-01-21 22:31:05 -0600

    rerun conversion after minor bugfix

--- apps/git/git.py
+++ apps/git/git.py
@@ -10,11 +10,6 @@ ctx = Context()
 mod.list("git_command", desc="Git commands.")
 mod.list("git_argument", desc="Command-line git options and arguments.")
 
-dirpath = Path(__file__).parent
-# arguments_csv_path = str(dirpath / "git_arguments.csv")
-# commands_csv_path = str(dirpath / "git_commands.csv")
-
-
 @mod.capture(rule="{user.git_argument}+")
 def git_arguments(m) -> str:
     """A non-empty sequence of git command arguments, preceded by a space."""
