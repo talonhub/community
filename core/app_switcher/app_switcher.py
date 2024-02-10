2b5db6dd6b0616b07cc3917aca24f43d1b706408
Nicholas Riley <com-github@sabi.net>
2024-02-10 10:36:53 -0500

    Update app switcher to work with public and beta Talon.
    To fix https://github.com/talonvoice/talon/issues/355, beta Talon uses
    lowercase for app.exe. As the app name overrides CSV allows you to
    override by app.exe on Windows, any overrides (including some defaults
    from community) will won't work on beta Talon, so lowercase everything.
    
    Also updates "running list" to be a more readable and implements the
    "todo" so overrides actually override spoken forms for an application.

--- core/app_switcher/app_switcher.py
+++ core/app_switcher/app_switcher.py
@@ -229,32 +229,34 @@ def update_running_list():
     global running_application_dict
     running_application_dict = {}
     running = {}
-    for cur_app in ui.apps(background=False):
-        running_application_dict[cur_app.name] = True
+    foreground_apps = ui.apps(background=False)
+
+    for cur_app in foreground_apps:
+        running_application_dict[cur_app.name.lower()] = cur_app.name
 
         if app.platform == "windows":
-            # print("hit....")
-            # print(cur_app.exe)
-            running_application_dict[cur_app.exe.split(os.path.sep)[-1]] = True
+            exe = os.path.split(cur_app.exe)[1]
+            running_application_dict[exe.lower()] = exe
+
+    override_apps = frozenset(overrides.values())
 
     running = actions.user.create_spoken_forms_from_list(
-        [curr_app.name for curr_app in ui.apps(background=False)],
+        [
+            curr_app.name
+            for curr_app in ui.apps(background=False)
+            if curr_app.name.lower() not in override_apps
+            and curr_app.exe.lower() not in override_apps
+            and os.path.basename(curr_app.exe).lower() not in override_apps
+        ],
         words_to_exclude=words_to_exclude,
         generate_subsequences=True,
     )
 
-    # print(str(running_application_dict))
-    # todo: should the overrides remove the other spoken forms for an application?
-    for override in overrides:
-        if overrides[override] in running_application_dict:
-            running[override] = overrides[override]
+    for running_name, full_application_name in overrides.items():
+        if running_app_name := running_application_dict.get(full_application_name):
+            running[running_name] = running_app_name
 
-    lists = {
-        "self.running": running,
-    }
-
-    # batch update lists
-    ctx.lists.update(lists)
+    ctx.lists["self.running"] = running
 
 
 def update_overrides(name, flags):
@@ -266,10 +268,10 @@ def update_overrides(name, flags):
         # print("update_overrides")
         with open(override_file_path) as f:
             for line in f:
-                line = line.rstrip()
+                line = line.rstrip().lower()
                 line = line.split(",")
                 if len(line) == 2:
-                    overrides[line[0].lower()] = line[1].strip()
+                    overrides[line[0]] = line[1].strip()
 
         update_running_list()
 
@@ -281,7 +283,7 @@ class Actions:
         # We should use the capture result directly if it's already in the list
         # of running applications. Otherwise, name is from <user.text> and we
         # can be a bit fuzzier
-        if name not in running_application_dict:
+        if name.lower() not in running_application_dict:
             if len(name) < 3:
                 raise RuntimeError(
                     f'Skipped getting app: "{name}" has less than 3 chars.'
@@ -297,7 +299,7 @@ class Actions:
         for application in ui.apps(background=False):
             if application.name == name or (
                 app.platform == "windows"
-                and application.exe.split(os.path.sep)[-1] == name
+                and os.path.split(application.exe)[1].lower() == name.lower()
             ):
                 return application
         raise RuntimeError(f'App not running: "{name}"')
@@ -380,10 +382,11 @@ class Actions:
 
 @imgui.open()
 def gui_running(gui: imgui.GUI):
-    gui.text("Names of running applications")
+    gui.text("Running applications (with spoken forms)")
     gui.line()
-    for line in ctx.lists["self.running"]:
-        gui.text(line)
+    running_apps = sorted((v, k) for k, v in ctx.lists["self.running"].items())
+    for full_application_name, running_name in running_apps:
+        gui.text(f"{full_application_name}: {running_name}")
 
     gui.spacer()
     if gui.button("Running close"):
