e0899adc38c5ea6f92c7aadb2e3dea5616c659f5
Miles Watson <contact@mileswatson.net>
2024-02-21 12:23:22 +0000

    Changed alt to alter.

--- core/keys/keys.py
+++ core/keys/keys.py
@@ -123,7 +123,7 @@ def letters(m) -> str:
 ctx = Context()
 modifier_keys = {
     # If you find 'alt' is often misrecognized, try using 'alter'.
-    "alt": "alt",  #'alter': 'alt',
+    "alter": "alt",  #'alter': 'alt',
     "control": "ctrl",  #'troll':   'ctrl',
     "shift": "shift",  #'sky':     'shift',
     "super": "super",
