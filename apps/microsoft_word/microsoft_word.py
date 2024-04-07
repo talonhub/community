dafcf353bedc89433ef51cc74b8d5a5c779ffd3c
Jeff Knaus <knaus.jeff@gmail.com>
2024-04-07 18:05:13 -0400

    Update microsoft_word.py

--- apps/microsoft_word/microsoft_word.py
+++ apps/microsoft_word/microsoft_word.py
@@ -9,6 +9,14 @@ and app.exe: /^chrome\.exe$/i
 win.title: /\.docx/
 win.title: /\.doc/
 """
+mod.apps.microsoft_word = r"""
+os: windows
+and app.name: Microsoft Edge
+os: windows
+and app.exe: /^msedge\.exe$/i
+win.title: /\.docx/
+win.title: /\.doc/
+"""
 
 # @mod.action_class
 # class Actions:
