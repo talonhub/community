ab226526701d5c0971b05681463bef0bd395996c
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-05-12 09:42:15 +0200

    Creates separate action to cancel current phrase

--- plugin/cancel/cancel.py
+++ plugin/cancel/cancel.py
@@ -3,12 +3,13 @@
 
 import time
 
-from talon import Context, actions, speech_system
+from talon import Context, Module, actions, speech_system
 from talon.grammar import Phrase
 
 # To change the phrase used to cancel commands, you must also adjust misc/cancel.talon
 cancel_phrase = "cancel cancel".split()
 
+mod = Module()
 ctx = Context()
 
 ts_threshold: float = 0
@@ -21,9 +22,16 @@ class SpeechActions:
     # started before wakeup. This is to prevent commands from being executed if
     # the user wakes Talon using a noise or keypress
     def enable():
+        actions.user.cancel_current_phrase()
+        actions.next()
+
+
+@mod.action_class
+class Actions:
+    def cancel_current_phrase():
+        """Cancel/abort current spoken phrase"""
         global ts_threshold
         ts_threshold = time.perf_counter()
-        actions.next()
 
 
 def pre_phrase(phrase: Phrase):
@@ -36,11 +44,11 @@ def pre_phrase(phrase: Phrase):
     # Check if the phrase is before the threshold
     if ts_threshold != 0:
         words = phrase["phrase"]
-        # Start of phrase is before timestamp threshold
         start = getattr(words[0], "start", phrase["_ts"])
-        delta = ts_threshold - start
+        phrase_starts_before_threshold = start < ts_threshold
         ts_threshold = 0
-        if delta > 0:
+        # Start of phrase is before threshold timestamp
+        if phrase_starts_before_threshold:
             abort_phrase(phrase)
             return
 
