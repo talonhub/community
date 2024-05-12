b9c001f150f4706e5a747a4f7dd318c399080728
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-05-12 17:58:01 +0200

    Message added

--- plugin/cancel/cancel.py
+++ plugin/cancel/cancel.py
@@ -38,20 +38,18 @@ class Actions:
 def pre_phrase(phrase: Phrase):
     global ts_threshold
 
-    # Nothing to cancel
-    if not phrase.get("phrase") or "parsed" not in phrase:
-        return
-
     # Check if the phrase is before the threshold
     if ts_threshold != 0:
         words = phrase["phrase"]
-        start = getattr(words[0], "start", phrase["_ts"])
-        phrase_starts_before_threshold = start < ts_threshold
-        ts_threshold = 0
-        # Start of phrase is before threshold timestamp
-        if phrase_starts_before_threshold:
-            abort_phrase(phrase)
-            return
+        if words:
+            start = getattr(words[0], "start", phrase["_ts"])
+            phrase_starts_before_threshold = start < ts_threshold
+            ts_threshold = 0
+            # Start of phrase is before threshold timestamp
+            if phrase_starts_before_threshold:
+                print(f"Aborted phrase: {''.join(words)}")
+                abort_entire_phrase(phrase)
+                return
 
     # Check if the phrase is a cancel command
     if "text" in phrase:
@@ -59,12 +57,14 @@ def pre_phrase(phrase: Phrase):
         before, after = phrase["text"][:-n], phrase["text"][-n:]
         if after == cancel_phrase:
             actions.app.notify(f"Command canceled: {' '.join(before)!r}")
-            abort_phrase(phrase)
+            abort_entire_phrase(phrase)
             return
 
 
-def abort_phrase(phrase: Phrase):
-    phrase["parsed"]._sequence = []
+def abort_entire_phrase(phrase: Phrase):
+    phrase["phrase"] = []
+    if "parsed" in phrase:
+        phrase["parsed"]._sequence = []
 
 
 speech_system.register("pre:phrase", pre_phrase)
