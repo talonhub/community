f6bb83d84f5b7038cc633e9bb2ae391044f66706
fidgetingbits <fidgetingbits@users.noreply.github.com>
2024-05-30 10:14:09 +0800

    expand tab actions

--- core/windows_and_tabs/tabs.py
+++ core/windows_and_tabs/tabs.py
@@ -1,6 +1,14 @@
-from talon import Module, actions, app
+from talon import Module, actions, settings
 
 mod = Module()
+
+mod = Module()
+mod.setting(
+    "tab_name_format",
+    type=str,
+    default="ALL_CAPS,DASH_SEPARATED",
+    desc="Default format of renamed tab names in an app",
+)
 
 
 @mod.action_class
@@ -19,3 +27,34 @@ class tab_actions:
 
     def tab_duplicate():
         """Duplicates the current tab."""
+
+    def tab_search():
+        """Searches through the tabs."""
+
+    def tab_pin():
+        """Pins the current tab."""
+
+    def tab_unpin():
+        """Unpins the current tab."""
+
+    def tab_rename_wrapper(name: str):
+        """Applies formatting to tab name prior to passing to overridden tab_rename()"""
+        if len(name):
+            name = actions.user.formatted_text(name, actions.user.tab_name_format())
+        actions.app.tab_rename(name)
+
+    def tab_rename(name: str):
+        """Renames the current tab."""
+
+    def tab_name_format():
+        """Formatters for tab names in given app"""
+        return settings.get("user.tab_name_format")
+
+    def tab_focus_most_recent():
+        """Jumps to the most recently viewed tab."""
+
+    def tab_move_left():
+        """Moves the current tab to the left."""
+
+    def tab_move_right():
+        """Moves the current tab to the right."""
