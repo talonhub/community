9e1c1e750ababd782c986498510ad88a31da5e33
max.bruening <56445556+maxbruening@users.noreply.github.com>
2024-06-05 12:10:17 +0200

    Explicitly declare stata app

--- apps/stata/stata.py
+++ apps/stata/stata.py
@@ -1,10 +1,18 @@
-from talon import Context
+from talon import Module, Context
 
+mod = Module()
 ctx = Context()
+
+mod.apps.stata = r"""
+os: windows
+and app.name: Stata
+os: windows
+and app.exe: /^statase\-64\.exe$/i
+"""
+
 ctx.matches = r"""
 app: stata
 """
-
 
 @ctx.action_class("code")
 class CodeActions:
