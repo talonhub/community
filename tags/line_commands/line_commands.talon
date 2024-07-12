fidgetingbits/fidgetingbits-talon

expand line command actions

tags/line_commands/line_commands.talon
@@ -70,3 +70,6 @@ select camel left: user.extend_camel_left()
 select camel right: user.extend_camel_right()
 go camel left: user.camel_left()
 go camel right: user.camel_right()
+
+go char <user.unmodified_key>: user.line_find_forward(unmodified_key)
+go last char <user.unmodified_key>: user.line_find_backward(unmodified_key)

talonhub/community

tags/line_commands/line_commands.talon
@@ -70,3 +70,6 @@ select camel left: user.extend_camel_left()
 select camel right: user.extend_camel_right()
 go camel left: user.camel_left()
 go camel right: user.camel_right()
+
+go char <user.unmodified_key>: user.jump_cursor_to_next_char(unmodified_key)
+go last char <user.unmodified_key>: user.jump_cursor_to_prev_char(unmodified_key)