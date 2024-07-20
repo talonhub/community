71d35e0b9105194c38494134e35b0de39f931705
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-20 12:14:08 +0200

    Implemented compound edit command

---
+++ core/edit/edit_command_actions.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass
+from typing import Callable, Union
+from talon import Module, actions
+
+
+@dataclass
+class EditSimpleAction:
+    type: str
+
+    def __str__(self):
+        return self.type
+
+
+@dataclass
+class EditInsertAction:
+    type = "insert"
+    text: str
+
+    def __str__(self):
+        return self.type
+
+
+@dataclass
+class EditFormatAction:
+    type = "applyFormatter"
+    formatters: str
+
+    def __str__(self):
+        return self.type
+
+
+EditAction = Union[
+    EditSimpleAction,
+    EditInsertAction,
+    EditFormatAction,
+]
+
+
+mod = Module()
+mod.list("edit_action", desc="Actions for the edit command")
+
+
+@mod.capture(rule="{user.edit_action}")
+def edit_simple_action(m) -> EditSimpleAction:
+    return EditSimpleAction(m.edit_action)
+
+
+@mod.capture(rule="<user.edit_simple_action>")
+def edit_action(m) -> EditAction:
+    return m[0]
+
+
+simple_action_callbacks: dict[str, Callable] = {
+    "select": actions.skip,
+    "goBefore": actions.edit.left,
+    "goAfter": actions.edit.right,
+    "copyToClipboard": actions.edit.copy,
+    "cutToClipboard": actions.edit.cut,
+    "pasteFromClipboard": actions.edit.paste,
+    "insertLineAbove": actions.edit.line_insert_up,
+    "insertLineBelow": actions.edit.line_insert_down,
+    "insertCopyAfter": actions.edit.selection_clone,
+    "delete": actions.edit.delete,
+}
+
+
+def get_action_callback(action: EditAction) -> Callable:
+    action_type = action.type
+
+    if action_type in simple_action_callbacks:
+        return simple_action_callbacks[action_type]
+
+    match action_type:
+        case "insert":
+            assert isinstance(action, EditInsertAction)
+            return lambda: actions.insert(action.text)
+
+        case "applyFormatter":
+            assert isinstance(action, EditFormatAction)
+            return lambda: actions.user.formatters_reformat_selection(action.formatters)
+
+    raise ValueError(f"Unknown edit action: {action}")
---
+++ core/edit/edit_command_modifiers.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import Callable
+from talon import Module, actions
+
+
+mod = Module()
+mod.list("edit_modifier", desc="Modifiers for the edit command")
+
+
+@dataclass
+class EditModifier:
+    type: str
+
+    def __str__(self):
+        return self.type
+
+
+@mod.capture(rule="{user.edit_modifier}")
+def edit_modifier(m) -> EditModifier:
+    return EditModifier(m.edit_modifier)
+
+
+modifier_callbacks = {
+    "document": actions.edit.select_all,
+    "paragraph": actions.edit.select_paragraph,
+    "word": actions.edit.select_word,
+    "line": actions.edit.select_line,
+    "lineEnd": actions.user.select_line_end,
+    "lineStart": actions.user.select_line_start,
+}
+
+
+def get_modifier_callback(modifier: EditModifier) -> Callable:
+    key = str(modifier)
+
+    if key in modifier_callbacks:
+        return modifier_callbacks[key]
+
+    raise ValueError(f"Unknown edit modifier: {modifier}")
