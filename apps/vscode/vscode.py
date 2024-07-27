c5a29be7cf3aab24e2483f8845e49310101bb178
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-27 19:04:52 +0200

    Replace `user.find` with `edit.find` (#1499)
    We should definitely not implement user namespace actions we're Talon
    already have defined them on internal namespaces.
    
    Fixes #1493
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Nicholas Riley <com-github@sabi.net>

--- apps/vscode/vscode.py
+++ apps/vscode/vscode.py
@@ -101,13 +101,11 @@ class EditActions:
     def save_all():
         actions.user.vscode("workbench.action.files.saveAll")
 
-    def find(text=None):
-        if is_mac:
-            actions.key("cmd-f")
-        else:
-            actions.key("ctrl-f")
-        if text is not None:
-            actions.insert(text)
+    def find_next():
+        actions.user.vscode("editor.action.nextMatchFindAction")
+
+    def find_previous():
+        actions.user.vscode("editor.action.previousMatchFindAction")
 
     def line_swap_up():
         actions.key("alt-up")
@@ -168,6 +166,11 @@ class Actions:
 class MacUserActions:
     def command_palette():
         actions.key("cmd-shift-p")
+
+    def find(text: str = None):
+        actions.key("cmd-f")
+        if text:
+            actions.insert(text)
 
 
 @ctx.action_class("user")
@@ -275,26 +278,6 @@ class UserActions:
                 actions.key(f"ctrl-{index}")
 
     # splits.py support end
-
-    # find.py support begin
-
-    def find(text: str):
-        """Triggers find in current editor"""
-        if is_mac:
-            actions.key("cmd-f")
-        else:
-            actions.key("ctrl-f")
-
-        if text:
-            actions.insert(text)
-
-    def find_next():
-        actions.user.vscode("editor.action.nextMatchFindAction")
-
-    def find_previous():
-        actions.user.vscode("editor.action.previousMatchFindAction")
-
-    # find.py support end
 
     # find_and_replace.py support begin
 
