d2c0393766c4f629c352de06f972e5691221a893
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-10-23 12:13:45 +0200

    Clean up emoji

--- tags/emoji/emoji.py
+++ tags/emoji/emoji.py
@@ -1,19 +1,8 @@
-from pathlib import Path
+from talon import Module
 
-from talon import Context, Module
-
-# --- Tag definition ---
 mod = Module()
+
 mod.tag("emoji", desc="Emoji, ascii emoticons and kaomoji")
-
-# Context matching
-ctx = Context()
-ctx.matches = """
-tag: user.emoji
-"""
-
-# --- Define and implement lists ---
-path = Path(__file__).parents[0]
 
 mod.list("emoticon", desc="Western emoticons (ascii)")
 mod.list("emoji", desc="Emoji (unicode)")
