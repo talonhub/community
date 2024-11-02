a7dc2d8231badffda59bacc333d63ed98cb51f77
Joshua Aresty <jaresty@gmail.com>
2024-11-01 23:27:11 -0700

    First pass at pull request feedback from today

--- core/windows_and_tabs/window_layout.py
+++ core/windows_and_tabs/window_layout.py
@@ -1,13 +1,14 @@
 import copy
 from dataclasses import dataclass
-from datetime import datetime
+from typing import List, Optional, Union
+from time import perf_counter
+
+from talon import Context, Module, actions, settings, ui
 
 """Tools for laying out windows in an arrangement """
 
-import logging
-from typing import Any, Dict, Optional
-
-from talon import Context, Module, actions, settings, ui
+from talon import Context, Module, actions, settings
+from talon.ui import Window, UIErr
 
 mod = Module()
 
@@ -16,17 +17,20 @@ mod.list(
     "Predefined window positions when splitting the screen between multiple windows.",
 )
 
-last_focus_was_done_by_snap_layout = False
-lay_out_start_time = datetime.now()
 
+def focus_callback(_):
+    global layout_in_progress
+    global last_layout
+    if layout_in_progress is not None:
+        return
 
-def _focus_callback(_):
-    global last_focus_was_done_by_snap_layout
-    global lay_out_start_time
-
-    if (datetime.now() - lay_out_start_time).total_seconds() >= 1:
+    delta = 1
+    if last_layout is not None:
+        delta = perf_counter() - last_layout.finish_time
+    print(f"delta ispr{delta}")
+    if delta >= 1:
         print("Resetting last focus")
-        last_focus_was_done_by_snap_layout = False
+        last_layout = None
 
 
 @dataclass
@@ -34,11 +38,16 @@ class WindowLayout:
     """Represents a layout of windows on a screen"""
 
     layout: list[str]
-    windows: list[Any]
+    windows: list[Window]
     should_rotate: bool
+    finish_time: float
 
 
-_split_positions = {
+layout_in_progress: Optional[WindowLayout] = None
+last_layout: Optional[WindowLayout] = None
+
+
+SPLIT_POSITIONS = {
     "split": {
         2: ["left", "right"],
         3: [
@@ -64,46 +73,47 @@ _split_positions = {
 }
 
 
-def _snap_next(windows: list[Any], target_layout: str) -> Optional[int]:
-    for index, window in enumerate(windows):
+def snap_next(windows: list[Window], target_layout: str) -> Optional[int]:
+    for idx, window in enumerate(windows):
         if window is None:
-            return index
+            return idx
         try:
             actions.user.snap_target_window_to_position(
                 window,
                 target_layout,
             )
-            return index
-        except Exception as e:
+
+            return idx
+        except UIErr as e:
             print(
-                f"Failed to snap window for application (this is normal) {window.app.name} {window.title}: {e}"
+                f'Failed to snap {window.app.name}\'s "{window.title}" window ({type(e).__name__} {e});  this is normal; continuing to the next'
             )
     return None
 
 
-def _snap_layout(window_layout: WindowLayout):
+def snap_layout(window_layout: WindowLayout):
     """Split the screen between multiple windows."""
-    global last_focus_was_done_by_snap_layout
-    global lay_out_start_time
+    global layout_in_progress
+    global last_layout
 
-    lay_out_start_time = datetime.now()
-
+    layout_in_progress = window_layout
     target_layout = copy.deepcopy(window_layout.layout)
     remaining_windows = window_layout.windows
     snapped_windows = []
-    snapped_window_index = 0
+    snapped_window_idx = 0
     if window_layout.should_rotate:
         print("Trying to rotate")
         target_layout.append(target_layout.pop(0))
 
     while len(target_layout) > 0:
-        snapped_window_index = _snap_next(remaining_windows, target_layout.pop(0))
-        if snapped_window_index is not None:
-            snapped_windows.insert(0, remaining_windows[snapped_window_index])
-            remaining_windows = remaining_windows[snapped_window_index + 1 :]
+        snapped_window_idx = snap_next(remaining_windows, target_layout.pop(0))
+        if snapped_window_idx is not None:
+            snapped_windows.insert(0, remaining_windows[snapped_window_idx])
+            remaining_windows = remaining_windows[snapped_window_idx + 1 :]
 
     if window_layout.should_rotate and len(snapped_windows) > 0:
         snapped_windows.append(snapped_windows.pop(0))
+
     for window in snapped_windows:
         try:
             actions.user.switcher_focus_window(window)
@@ -112,31 +122,32 @@ def _snap_layout(window_layout: WindowLayout):
                 f"Failed to focus window for application {window.app.name} {window.title}: {ex}"
             )
 
-    last_focus_was_done_by_snap_layout = True
+    layout_in_progress.finish_time = perf_counter()
+    last_layout = layout_in_progress
+    layout_in_progress = None
 
 
-def _windows_under_active_window() -> list[Any]:
+def filter_nonviable_windows(windows: List[Window]) -> list[Window]:
     active_window = ui.active_window()
-    ui_windows = ui.windows()
-    try:
-        active_index = ui_windows.index(active_window)
-        return ui_windows[active_index:]
-    except Exception:
-        return ui_windows
+
+    # Many invisible non-resizable windows are identifiable because they exist above the current window
+    # in the z-index
+    active_window_idx = windows.index(active_window)  # type: ignore
+    return list(filter(lambda w: windows.index(w) >= active_window_idx, windows))
 
 
 @mod.capture(rule="all")
-def all_candidate_windows(m) -> list[Any]:
-    return _windows_under_active_window()
+def all_candidate_windows(m) -> list[Window]:
+    return filter_nonviable_windows(ui.windows())
 
 
 @mod.capture(rule="gap")
-def skip_window(m) -> list[Any]:
+def skip_window(m) -> list[Window]:
     return [None]
 
 
 @mod.capture(rule="<user.running_applications>")
-def application_windows(m) -> list[Any]:
+def application_windows(m) -> list[Window]:
     return [
         window
         for app in m.running_applications_list
@@ -147,15 +158,16 @@ def application_windows(m) -> list[Any]:
 @mod.capture(
     rule="<user.application_windows>|<user.numbered_windows>|<user.skip_window>"
 )
-def layout_item(m) -> list[Any]:
+def layout_item(m) -> list[Union[Window, None]]:
     # Check for multiple attributes and raise an error if found
-    attributes = [
-        hasattr(m, "application_windows"),
-        hasattr(m, "numbered_windows"),
-        hasattr(m, "skip_window"),
-    ]
 
-    if sum(attributes) > 1:
+    attributes = [
+        "application_windows",
+        "numbered_windows",
+        "skip_window",
+    ]
+    num_passed = len(list(filter(lambda attrs: hasattr(m, attrs), attributes)))
+    if num_passed > 1:
         raise ValueError(
             "Multiple attributes found on 'm'. Only one of 'application_windows', 'numbered_windows', or 'skip_window' should be present."
         )
@@ -172,8 +184,8 @@ def layout_item(m) -> list[Any]:
 
 
 @mod.capture(rule="<user.ordinals_small>+")
-def numbered_windows(m) -> list[Any]:
-    all_windows = _windows_under_active_window()
+def numbered_windows(m) -> list[Window]:
+    all_windows = filter_nonviable_windows(ui.windows())
     selected_windows = [
         all_windows[i - 1] for i in m.ordinals_small_list if i - 1 < len(all_windows)
     ]
@@ -181,7 +193,7 @@ def numbered_windows(m) -> list[Any]:
 
 
 @mod.capture(rule="<user.layout_item>+ [<user.all_candidate_windows>]")
-def target_windows(m) -> list[Any]:
+def target_windows(m) -> list[Window]:
     windows = []
     if hasattr(m, "layout_item_list"):
         windows += [window for sublist in m.layout_item_list for window in sublist]
@@ -191,21 +203,21 @@ def target_windows(m) -> list[Any]:
     return windows
 
 
-def _pick_split_arrangement(
+def pick_split_arrangement(
     target_windows, window_split_positions, number_small
 ) -> list[str]:
     if target_windows is not None:
         target_length = len(target_windows)
     else:
-        target_length = len(_windows_under_active_window())
+        target_length = len(filter_nonviable_windows(ui.windows()))
     if number_small is not None:
-        return _split_positions[window_split_positions][number_small]
+        return SPLIT_POSITIONS[window_split_positions][number_small]
     else:
         closest_key = min(
-            _split_positions[window_split_positions].keys(),
+            SPLIT_POSITIONS[window_split_positions].keys(),
             key=lambda k: abs(k - target_length),
         )
-        return _split_positions[window_split_positions][closest_key]
+        return SPLIT_POSITIONS[window_split_positions][closest_key]
 
 
 @mod.capture(
@@ -216,23 +228,29 @@ def window_layout(m) -> WindowLayout:
     window_was_specified = hasattr(m, "target_windows")
     specified_layout_count = m.number_small if hasattr(m, "number_small") else None
     target_windows = (
-        m.target_windows if window_was_specified else _windows_under_active_window()
+        m.target_windows
+        if window_was_specified
+        else filter_nonviable_windows(ui.windows())
     )
+    print(f"target windows is {target_windows}")
 
-    layout = _pick_split_arrangement(
+    layout = pick_split_arrangement(
         target_windows, m.window_split_positions, specified_layout_count
     )
 
     return WindowLayout(
         layout,
         target_windows,
-        not window_was_specified and last_focus_was_done_by_snap_layout,
+        not window_was_specified
+        and last_layout is not None
+        and perf_counter() - last_layout.finish_time > 1,
+        0,
     )
 
 
 ctx = Context()
 
-ctx.lists["user.window_split_positions"] = _split_positions.keys()
+ctx.lists["user.window_split_positions"] = SPLIT_POSITIONS.keys()
 
 
 @mod.action_class
@@ -241,8 +259,8 @@ class Actions:
         window_layout: WindowLayout,
     ):
         """Split the screen between multiple applications."""
-        _snap_layout(window_layout)
+        snap_layout(window_layout)
 
 
-ui.register("app_activate", _focus_callback)
-ui.register("win_focus", _focus_callback)
+ui.register("app_activate", focus_callback)
+ui.register("win_focus", focus_callback)
