0dd9478a67c709aa0856586dfb72fa71ee57797f
thetomcraig <thetomcraig@users.noreply.github.com>
2024-11-02 09:13:10 -0700

    Add mission control to focus for app switcher (#1542)
    This adds functionality for application switching on MacOS. The actual
    equivalent on MacOS is the Application Switcher (Command + Tab), but
    there is no built in way for this, to be persistent on screen with a
    single hot key. The best alternative is Mission Control, which will show
    all of the open windows.
    
    ---------
    
    Co-authored-by: Tom Craig <147539579+thetomcraig-aya@users.noreply.github.com>
    Co-authored-by: Nicholas Riley <com-github@sabi.net>

--- core/app_switcher/app_switcher.py
+++ core/app_switcher/app_switcher.py
@@ -399,6 +399,9 @@ class Actions:
         """Open a menu of running apps to switch to"""
         if app.platform == "windows":
             actions.key("alt-ctrl-tab")
+        elif app.platform == "mac":
+            # MacOS equivalent is "Mission Control"
+            actions.user.dock_send_notification("com.apple.expose.awake")
         else:
             print("Persistent Switcher Menu not supported on " + app.platform)
 
