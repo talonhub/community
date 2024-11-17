47d4ee71b254bdf2d5ae418c7cd50e4c3cf84972
Jeff Knaus <knaus.jeff@gmail.com>
2024-11-16 20:38:05 -0700

    tweaks

--- personal/apps/mac/iphone_mirroring/iphone_mirroring.py
+++ personal/apps/mac/iphone_mirroring/iphone_mirroring.py
@@ -4,3 +4,13 @@ mod.apps.i_phone_mirroring = """
 os: mac
 and app.bundle: com.apple.ScreenContinuity
 """
+mod.list(
+    "phone_applications",
+    desc="list of known iPhone applications",
+)
+ctx = Context()
+
+ctx.lists["user.phone_applications"] = {
+    "github": "github", 
+    "messages": "messages", 
+}
