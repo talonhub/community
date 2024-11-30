6c94e5696e5264e14ac1d11fc8d98eab5ac35aa8
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-11-30 18:30:59 +0100

    Don't place cursor inside delimiters when wrapping

--- core/edit/insert_between.py
+++ core/edit/insert_between.py
@@ -5,8 +5,8 @@ mod = Module()
 
 @mod.action_class
 class module_actions:
-    def insert_between(before: str, after: str, middle: str = ""):
+    def insert_between(before: str, after: str):
         """Insert `before + after`, leaving cursor between `before` and `after`. Not entirely reliable if `after` contains newlines."""
-        actions.insert(f"{before}{middle}{after}")
+        actions.insert(f"{before}{after}")
         for _ in after:
             actions.edit.left()
