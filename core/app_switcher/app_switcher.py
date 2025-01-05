b0c046f762e23af257e4a72c50fa678abe9bbdd5
Jeff Knaus <knaus.jeff@gmail.com>
2025-01-04 22:15:14 -0700

    tweaks

--- core/app_switcher/app_switcher.py
+++ core/app_switcher/app_switcher.py
@@ -13,6 +13,10 @@ from .exclusion import ExclusionType, RunningApplicationExclusion
 from typing import Union
 from .application import Application, ApplicationGroup
 import re
+from talon.ui import Rect
+from talon.windows.ax import Element
+from dataclasses import dataclass
+
 import platform
 hostname = platform.node()
 base_script_directory = os.path.dirname(os.path.realpath(__file__))
@@ -35,6 +39,7 @@ mod = Module()
 mod.tag("app_switcher_selector_showing", desc="Indicates the app_switcher_selector_showing")
 mod.list("running", desc="all running applications")
 mod.list("launch", desc="all launchable applications")
+mod.list("running_applications", desc="all running applications")
 
 ctx = Context()
 
@@ -602,7 +607,12 @@ def update_launch_applications(f):
         print(f"Missing or new application detected, updating {launch_applications_filename}")
         process_launch_applications_file(True)
     else:
-        update_running_list()
+
+        if app.platform != "windows":
+            update_running_list()
+        else:
+            rebuild_taskbar_app_list()
+
         update_launch_list()
 
 @mod.action_class
@@ -754,6 +764,8 @@ class Actions:
         """Hides list of running applications"""
         gui_running.hide()
 
+def get_app_overrides():
+    return APPLICATIONS_OVERRIDES
 
 @imgui.open()
 def gui_running(gui: imgui.GUI):
@@ -792,9 +804,69 @@ def update_launch_list():
     result.update(customized)
     ctx.lists["self.launch"] = result
 
+cache = []
+
+@dataclass
+class TaskBarItem:
+    title: str
+    type: str
+    element: Element
+
+@mod.action_class
+class Actions:
+    def switcher_accessibility_focus(title: str):
+        """experimental"""
+        rebuild_taskbar_app_list()
+        for item in cache:
+            if item.title == title:
+                item.element.invoke_pattern.invoke()
+                break
+
+    # def switcher_accessibility_show():
+    #     """what"""
+    #     create_canvases()
+
+def first_matching_child(element, **kw):
+    if len(kw) > 1:
+        raise Exception("Only one matching attribute supported")
+    attr, values = list(kw.items())[0]
+    return next(e for e in element.children if getattr(e, attr) in values)
+
+def rebuild_taskbar_app_list(forced: bool = False):
+    global cache
+
+    explorer = ui.apps(name="Windows Explorer")[0]
+    taskbar = next(
+        window for window in explorer.windows() if window.cls == "Shell_TrayWnd"
+    )
+    running_applications = first_matching_child(taskbar.element, class_name=["MSTaskListWClass"])
+    #update_canvas = forced or len(running_applications.children) != len(cache)
+    cache = []
+    result = {}
+    for e in running_applications.children:
+
+        title = e.name
+        splits = title.split(" - ")
+        name = splits[0] 
+        cache.append(TaskBarItem(title, str(e.control_type), e))
+        override = None
+        if name in APPLICATIONS_OVERRIDES:
+            override = APPLICATIONS_OVERRIDES[name]
+            
+        if override and override.spoken_forms:
+            for form in override.spoken_forms:
+                result[form] = title
+        else:
+            result[name] = title
+
+    ctx.lists["user.running_applications"] = result
+
 def ui_event(event, arg):
     if event in ("app_launch", "app_close", "app_activate", "app_deactivate"):
-        update_running_list()
+        if app.platform != "windows":
+            update_running_list()
+        else:
+            rebuild_taskbar_app_list()
 
 def on_ready():
     ui.register("", ui_event)
