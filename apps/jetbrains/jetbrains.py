00016847b273105e593651246a3559269c747c8c
Anders Søe <anders@soee.org>
2025-01-17 07:46:36 +0100

    add macos matchers for more products. add matchers for local plugin development

--- apps/jetbrains/jetbrains.py
+++ apps/jetbrains/jetbrains.py
@@ -56,6 +56,8 @@ port_mapping = {
     "pycharm64.exe": 8658,
     "WebStorm": 8663,
     "webstorm64.exe": 8663,
+    # Local plugin development:
+    "com.jetbrains.jbr.java": 8666,
 }
 
 
@@ -122,8 +124,18 @@ mod.apps.jetbrains = r"app.exe: /^webstorm64\.exe$/i"
 mod.apps.jetbrains = """
 os: mac
 and app.bundle: com.jetbrains.pycharm
+"""
+mod.apps.jetbrains = """
 os: mac
 and app.bundle: com.jetbrains.rider
+"""
+mod.apps.jetbrains = """
+os: mac
+and app.bundle: com.jetbrains.goland
+"""
+mod.apps.jetbrains = """
+os: mac
+and app.bundle: com.jetbrains.intellij.ce
 """
 mod.apps.jetbrains = r"""
 os: windows
@@ -132,6 +144,11 @@ os: windows
 and app.exe: /^rider64\.exe$/i
 """
 
+# Local plugin development:
+mod.apps.jetbrains = """
+os: mac
+and app.bundle: com.jetbrains.jbr.java
+"""
 
 @mod.action_class
 class Actions:
