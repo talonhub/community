9f76955639fc157b5275a1877e4a1791ea988992
Jeff Knaus <knaus.jeff@gmail.com>
2025-01-19 13:01:46 -0700

    more progress

--- core/edit/edit_command.py
+++ core/edit/edit_command.py
@@ -1,30 +1,109 @@
-from talon import Module, actions
+from talon import Module, actions, settings
 
 from .edit_command_actions import EditAction, run_action_callback
 from .edit_command_modifiers import EditModifier, run_modifier_callback
 
+mod = Module()
+mod.setting(
+    "edit_command_word_selection_delay",
+    type=int,
+    default=75,
+    desc="Sleep required between word selections",
+)
+
+mod.setting(
+    "edit_command_line_selection_delay",
+    type=int,
+    default=75,
+    desc="Sleep required between line selections",
+)
 
 def before_line_up():
     actions.edit.up()
     actions.edit.line_start()
 
-
 def after_line_up():
     actions.edit.up()
     actions.edit.line_end()
-
 
 def before_line_down():
     actions.edit.down()
     actions.edit.line_start()
 
-
 def after_line_down():
     actions.edit.down()
     actions.edit.line_end()
 
+def action_handler(action):
+    if action == "selection":
+        pass
+    elif action == "cutToClipboard":
+        actions.edit.cut()
+    elif action == "copyToClipboard":
+        actions.edit.copy()
+    elif action in ["delete", "deleteLeft", "deleteRight"]:
+        actions.edit.delete()
 
-# In some cases there already is a "compound" talon action for a given action and modifier
+def select_lines(action, direction, count):
+    if direction == "lineUp":
+        selection_callback = actions.edit.extend_line_up
+    else:
+        selection_callback = actions.edit.extend_line_down
+
+    selection_delay = f"{settings.get('user.edit_command_word_selection_delay')}ms"
+
+    for i in range(1, count + 1):
+        selection_callback()
+        actions.sleep(selection_delay)
+    
+    # ensure we take the start of the line too!
+    actions.edit.extend_line_start()
+    actions.sleep(selection_delay)
+    action_handler(action)
+
+def select_words(action, direction, count):
+    if direction == "wordLeft":
+        selection_callback = actions.edit.extend_word_left
+    else:
+        selection_callback = actions.edit.extend_word_right
+
+    selection_delay = f"{settings.get('user.edit_command_word_selection_delay')}ms"
+    for i in range(1, count + 1):
+        selection_callback()
+        actions.sleep(selection_delay)
+
+    action_handler(action)
+
+# in some cases, it is necessary to have some custom handling for timing reasons
+custom_callbacks = {
+    # delete
+    ("delete", "word"): select_words,
+    ("delete", "wordLeft"): select_words,
+    ("deleteLeft", "wordLeft"): select_words,
+    ("deleteRight", "wordRight"): select_words,
+    ("deleteLeft", "wordRight"): select_words,
+    ("delete", "wordRight"): select_words,
+    ("delete", "lineUp"): select_lines,
+    ("deleteLeft", "lineUp"): select_lines,
+    ("deleteRight", "lineUp"): select_lines,
+    ("delete", "lineDown"): select_lines,
+    ("deleteLeft", "lineDown"): select_lines,
+    ("deleteRight", "lineDown"): select_lines,
+
+    #cut
+    ("cutToClipboard", "word"): select_words,
+    ("cutToClipboard", "wordLeft"): select_words,
+    ("cutToClipboard", "wordRight"): select_words,
+    ("cutToClipboard", "lineUp"): select_lines,
+    ("copyToClipboard", "lineDown"): select_lines,
+
+    #copy
+    ("copyToClipboard", "word"): select_words,
+    ("copyToClipboard", "wordLeft"): select_words,
+    ("copyToClipboard", "wordRight"): select_words,
+}
+
+# In other cases there already is a "compound" talon action for a given action and modifier
 compound_actions = {
     # selection
     ("selection", "wordLeft"): actions.edit.extend_word_left,
@@ -56,11 +135,9 @@ compound_actions = {
     # Delete
     ("delete", "left"): actions.edit.delete,
     ("delete", "right"): actions.user.delete_right,
-    ("delete", "word"): actions.edit.delete_word,
-    ("deleteLeft", "word"): actions.edit.delete_word,
     ("delete", "line"): actions.edit.delete_line,
     ("delete", "paragraph"): actions.edit.delete_paragraph,
-    # ("delete", "document"): actions.edit.delete_all, # Beta only
+    ("delete", "document"): actions.edit.delete_all,
     ("delete", "selection"): actions.edit.delete,
     # Cut to clipboard
     ("cutToClipboard", "line"): actions.user.cut_line,
@@ -69,19 +146,24 @@ compound_actions = {
     ("copyToClipboard", "selection"): actions.edit.copy,
 }
 
-mod = Module()
-
 
 @mod.action_class
 class Actions:
     def edit_command(action: EditAction, modifier: EditModifier):
         """Perform edit command"""
-        key = (action.type, modifier.type)
-        if key in compound_actions:
-            count = modifier.count
+        key = (action.type, modifier.type)   
+        count = modifier.count 
+
+        if key in custom_callbacks:
+            custom_callbacks[key](action.type, modifier.type, count)
+            return 
+        
+        elif key in compound_actions:
             for i in range(1, count + 1):
                 compound_actions[key]()
             return
-
+        
         run_modifier_callback(modifier)
         run_action_callback(action)
+
+    
