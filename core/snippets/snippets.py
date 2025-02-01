8580d3f18311194c48080a87f954cf829372b8c0
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-02-01 18:58:50 +0100

    Clean up

--- core/snippets/snippets.py
+++ core/snippets/snippets.py
@@ -177,10 +177,10 @@ def create_lists(
 
 
 def on_ready():
-    fs.watch(SNIPPETS_DIR, lambda _1, _2: update_snippets())
+    fs.watch(SNIPPETS_DIR, lambda _path, _flags: update_snippets())
 
     if get_setting_dir():
-        fs.watch(str(get_setting_dir()), lambda _1, _2: update_snippets())
+        fs.watch(get_setting_dir(), lambda _path, _flags: update_snippets())
 
     update_snippets()
 
