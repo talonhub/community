034e54b7d6a92c9676f7fd897fc1e8bff89de137
Jeff Knaus <knaus.jeff@gmail.com>
2025-02-08 10:42:18 -0700

    Update edit_navigation_steps.py

--- core/edit/edit_navigation_steps.py
+++ core/edit/edit_navigation_steps.py
@@ -33,14 +33,11 @@ class Actions:
         for step in steps:
             match step.type:
                 case "wordLeft":
-                    delay = f"{settings.get('user.edit_command_word_selection_delay')}ms"
-                    repeat_action(actions.edit.word_left, step.count, delay)
+                    repeat_action(actions.edit.word_left, step.count, True)
                 case "wordRight":
-                    delay = f"{settings.get('user.edit_command_word_selection_delay')}ms"
-                    repeat_action(actions.edit.word_right, step.count, delay)
+                    repeat_action(actions.edit.word_right, step.count, True)
                 case "word":
-                    delay = f"{settings.get('user.edit_command_word_selection_delay')}ms"
-                    repeat_action(actions.edit.word_right, step.count, delay)
+                    repeat_action(actions.edit.word_right, step.count, True)
                 case "left":
                     repeat_action(actions.edit.left, step.count)
                 case "right":
@@ -51,9 +48,14 @@ class Actions:
                     repeat_action(actions.edit.down, step.count)
 
 
-def repeat_action(action: Callable, count: int, delay_before_next_action: str = None):
+def repeat_action(action: Callable, count: int, delay: bool = False):
+    delay_string = None
+
+    if delay:
+        delay_string = f"{settings.get('user.edit_command_word_selection_delay')}ms" 
+
     for _ in range(count):
         action()
         
-        if delay_before_next_action:
-            actions.sleep(delay_before_next_action)
+        if delay_string:
+            actions.sleep(delay_string)
