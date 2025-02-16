4bc228bd74fcfd36943c1984818d95a3ecdc9d2c
Fire Chicken <firechickenproductivity@gmail.com>
2025-02-15 19:18:13 -0700

    Draft elixir migration

--- lang/elixir/elixir.py
+++ lang/elixir/elixir.py
@@ -1,5 +1,6 @@
 from talon import Context, Module, actions, settings
 
+from ..tags.operators import Operators
 ctx = Context()
 mod = Module()
 ctx.matches = r"""
@@ -33,53 +34,32 @@ ctx.lists["user.code_keyword"] = {
     "use": "use ",
 }
 
+operators = Operators(
+    LAMBDA="->",
+
+    ASSIGNMENT=" = ",
+
+    MATH_ADD=" + ",
+    MATH_SUBTRACT=" - ",
+    MATH_MULTIPLY=" * ",
+    MATH_DIVIDE=" / ",
+    MATH_EQUAL=" == ",
+    MATH_NOT_EQUAL=" != ",
+    MATH_GREATER_THAN=" > ",
+    MATH_GREATER_THAN_OR_EQUAL=" >= ",
+    MATH_LESS_THAN=" < ",
+    MATH_LESS_THAN_OR_EQUAL=" <= ",
+    MATH_AND=" and ",
+    MATH_OR=" or ",
+)
 
 @ctx.action_class("user")
 class UserActions:
+    def code_get_operators() -> Operators:
+        return operators
+
     def code_comment_line_prefix():
         actions.insert("# ")
-
-    def code_operator_lambda():
-        actions.auto_insert("->")
-
-    def code_operator_assignment():
-        actions.auto_insert(" = ")
-
-    def code_operator_addition():
-        actions.auto_insert(" + ")
-
-    def code_operator_subtraction():
-        actions.auto_insert(" - ")
-
-    def code_operator_multiplication():
-        actions.auto_insert(" * ")
-
-    def code_operator_division():
-        actions.auto_insert(" / ")
-
-    def code_operator_equal():
-        actions.auto_insert(" == ")
-
-    def code_operator_not_equal():
-        actions.auto_insert(" != ")
-
-    def code_operator_greater_than():
-        actions.auto_insert(" > ")
-
-    def code_operator_greater_than_or_equal_to():
-        actions.auto_insert(" >= ")
-
-    def code_operator_less_than():
-        actions.auto_insert(" < ")
-
-    def code_operator_less_than_or_equal_to():
-        actions.auto_insert(" <= ")
-
-    def code_operator_and():
-        actions.auto_insert(" and ")
-
-    def code_operator_or():
-        actions.auto_insert(" or ")
 
     def code_self():
         actions.auto_insert("self")
