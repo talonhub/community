b5da169015c17845909a1271e7f3b7bdbce7b5a3
Jeff Knaus <knaus.jeff@gmail.com>
2025-03-23 09:32:02 -0600

    Add missing comma

--- core/keys/symbols.py
+++ core/keys/symbols.py
@@ -78,7 +78,7 @@ symbols = [
     Symbol("/", ["forward slash"], ["slash"]),
     Symbol("\\", None, ["backslash"]),
     Symbol("{", None, ["brace", "L brace", "left brace", "curly bracket", "left curly bracket"],),
-    Symbol("}", None, ["R brace", "right brace" "R curly bracket", "right curly bracket"]),
+    Symbol("}", None, ["R brace", "right brace","R curly bracket", "right curly bracket"]),
     Symbol("<", None, ["angle", "L Angle", "left angle", "less than"]),
     Symbol(">", None, ["rangle", "R angle", "right angle", "greater than"]),
     Symbol("^", None, ["caret"]),
