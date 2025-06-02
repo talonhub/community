4880c2f93679c1e1f98bf7b2f473e22f73f2fd98
Fire Chicken <firechickenproductivity@gmail.com>
2025-06-02 11:23:20 -0600

    Support customizing indentation with raw text insert

--- core/snippets/snippets_insert_raw_text.py
+++ core/snippets/snippets_insert_raw_text.py
@@ -1,9 +1,24 @@
 import re
 from dataclasses import dataclass
 
-from talon import actions
+from talon import actions, settings, Module
 
-INDENTATION = "    "
+mod = Module()
+
+mod.setting(
+    'snippets_raw_text_convert_indentation',
+    type = bool,
+    default = True,
+    desc = """If true, snippets inserted as raw text have tabs converted to spaces"""
+)
+
+mod.setting(
+    'snippets_raw_text_spaces_per_tab',
+    type = int,
+    default = 4,
+    desc = """The number of spaces to use for each tab in snippets inserted as raw text when converting tabs to spaces"""
+)
+
 RE_STOP = re.compile(r"\$(\d+|\w+)|\$\{(\d+|\w+)\}|\$\{(\d+|\w+):(.+)\}")
 
 
@@ -26,10 +41,13 @@ def insert_snippet_raw_text(body: str):
         actions.edit.line_start()
         right(stop.col)
 
+def compute_indentation_as_spaces():
+    return " "*settings.get("user.snippets_raw_text_spaces_per_tab")
 
 def parse_snippet(body: str):
     # Some IM services will send the message on a tab
-    body = re.sub(r"\t", INDENTATION, body)
+    if settings.get("user.snippets_raw_text_convert_indentation"):
+        body = re.sub(r"\t", compute_indentation_as_spaces(), body)
 
     # Replace variable with appropriate value/text
     body = re.sub(r"\$TM_SELECTED_TEXT", lambda _: actions.edit.selected_text(), body)
