19bb259d622fc12c4963e86cf69ae851c6fef30c
Trillium Smith <trillium@trilliumsmith.com>
2025-06-06 19:01:23 -0700

    refactor: Combine import statements
    Co-authored-by: Andreas Arvidsson <andreas.arvidsson87@gmail.com>

--- plugin/mode_indicator/mode_indicator.py
+++ plugin/mode_indicator/mode_indicator.py
@@ -3,8 +3,7 @@ from talon.canvas import Canvas
 from talon.screen import Screen
 from talon.skia.canvas import Canvas as SkiaCanvas
 from talon.skia.imagefilter import ImageFilter
-from talon.types.point import Point2d
-from talon.ui import Rect
+from talon.ui import Rect, Point2d
 
 canvas: Canvas = None
 current_mode = ""
