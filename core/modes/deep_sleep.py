6fccc3ccc5e971e269c34e2a642a99ec8aadc5ab
Nicholas Riley <com-github@sabi.net>
2025-06-21 12:50:24 -0400

    Update core/modes/deep_sleep.py

--- core/modes/deep_sleep.py
+++ core/modes/deep_sleep.py
@@ -13,8 +13,8 @@ ctx = Context()
 class Actions:
     def deep_sleep_enable():
         """Enable deep sleep.
-        Deep sleep has the advantage of requiring a longer wakeup command to exit sleep mode, which can help prevent unintended wakeups from conversations, meetings, listening to videos, etc.
-        Community does not come with a built in command for activating deep sleep. The expected way to use it is enabling the "user.deep_sleep" tag in applications where unwanted wakeups are more likely or problematic like meeting apps. Users can still enable the action with a command by creating a custom command using this action.
+        Deep sleep requires a longer wakeup command to exit sleep mode (defined in `sleep_mode_deep.talon`), which can help prevent unintended wakeups from conversations, meetings, listening to videos, etc. Instead of invoking this action directly, consider enabling the `user.deep_sleep` tag in applications where unwanted wakeups are more likely or problematic, such as meeting apps. With this tag active, any sleep command triggers deep sleep.
+        Users can also manually activate deep sleep by defining a custom voice command using this action.
         Note that when activating the tag on a per application basis, you must remember to explicitly match sleep mode, as command mode is implicitly matched by default.
         Note: If waking up is not done with the user.deep_sleep_disable action, then the deep sleep tag will stay active.
         """
