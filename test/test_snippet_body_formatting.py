068bdb648a2558bd2f7a313f08633ae19d061e66
Fire Chicken <firechickenproductivity@gmail.com>
2025-07-11 09:14:42 -0600

    Add more tests

--- test/test_snippet_body_formatting.py
+++ test/test_snippet_body_formatting.py
@@ -21,3 +21,37 @@ if hasattr(talon, "test_mode"):
         body = "[$1]($2)"
         expected = "[$1]($2)$0"
         assert_body_with_final_stop_added_as_expected(body, expected)
+
+    def test_empty_body_unchanged():
+        body = ""
+        assert_body_with_final_stop_added_as_expected(body, body)
+
+    def test_number_inside_braces():
+        body = "if (${1}){\n\t${0}}"
+        expected = "if (${1}){\n\t${2}}$0"
+        assert_body_with_final_stop_added_as_expected(body, expected)
+
+    def test_final_stop_with_default_value():
+        body = "import ${0:default_value}"
+        assert_body_with_final_stop_added_as_expected(body, body)
+
+    def test_final_stop_with_value_inside_braces():
+        body = "import ${0}"
+        assert_body_with_final_stop_added_as_expected(body, body)
+
+    def test_biggest_value_having_default():
+        body = "from ${1:module} import $0"
+        expected = "from ${1:module} import ${2}$0"
+        assert_body_with_final_stop_added_as_expected(body, expected)
+
+    def test_three_stops():
+        body = "[$0 for $2 in $1]"
+        expected = "[$3 for $2 in $1]$0"
+        assert_body_with_final_stop_added_as_expected(body, expected)
+
+    def test_multiple_final_stops():
+        body = "[$0 for $0 in $1 if $2]"
+        expected = "[$3 for $3 in $1 if $2]$0"
+        assert_body_with_final_stop_added_as_expected(body, expected)
+
+        
