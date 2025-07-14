2ea8caf665146c4f7ca746366fe6acef2ddee9de
NLCas8 <cas.wiefferink@nedap.com>
2025-07-14 11:31:17 +0200

    Replace constants with settings

--- apps/shortcat/shortcat.py
+++ apps/shortcat/shortcat.py
@@ -1,19 +1,27 @@
-from talon import Module, actions
+from talon import Module, actions, settings
 
 mod = Module()
 
-# Hotkey that is configured to activate Shortcat
-SHORTCAT_HOTKEY = "cmd-shift-space"
-# Delay to wait for Shortcat to show the shortcuts
-SHORTCUTS_DELAY = "500ms"
+mod.setting(
+    "shortcat_hotkey",
+    type=str,
+    default="cmd-shift-space",
+    desc="Hotkey that is configured to activate Shortcat",
+)
+mod.setting(
+    "shortcat_click_delay",
+    type=str,
+    default="500ms",
+    desc="Delay to wait for Shortcat to show the shortcuts before clicking",
+)
 
 
 @mod.action_class
 class Actions:
     def shortcat_hover(text: str):
         "Hover over a button using shortcat"
-        actions.key(SHORTCAT_HOTKEY)
-        actions.sleep(SHORTCUTS_DELAY)
+        actions.key(settings.get("user.shortcat_hotkey"))
+        actions.sleep(settings.get("user.shortcat_click_delay"))
         actions.insert(text)
 
     def shortcat_click(text: str, click_delay: str = "0ms"):
