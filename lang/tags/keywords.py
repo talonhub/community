4f29a6a29955ef654071159842d2870f0b5dc624
Fire Chicken <firechickenproductivity@gmail.com>
2025-07-26 20:02:53 -0600

    Improve efficiency

--- lang/tags/keywords.py
+++ lang/tags/keywords.py
@@ -25,8 +25,20 @@ class Actions:
         else:
             # every keyword is separated by a space
             # the spacing before the first keyword and after the last keyword is kept
-            combined_text: str = keywords[0].rstrip() + " "
-            for i in range(1, len(keywords) - 1):
-                combined_text += keywords[i].strip() + " "
-            combined_text += keywords[-1].lstrip()
+            num_leading_spaces = compute_number_of_leading_spaces(keywords[0])
+            num_trailing_spaces = compute_number_of_trailing_spaces(keywords[-1])
+            stripped_words = [word.strip() for word in keywords]
+            combined_text = " "*num_leading_spaces + " ".join(stripped_words) + " "*num_trailing_spaces
             actions.insert(combined_text)
+
+def compute_number_of_leading_spaces(text: str) -> int:
+    num_leading_spaces = 0
+    while text[num_leading_spaces] == " " and len(text) > num_leading_spaces:
+        num_leading_spaces += 1
+    return num_leading_spaces
+
+def compute_number_of_trailing_spaces(text: str) -> int:
+    num_trailing_spaces = 0
+    while text[-(num_trailing_spaces + 1)] == " " and len(text) > num_trailing_spaces:
+        num_trailing_spaces += 1
+    return num_trailing_spaces
