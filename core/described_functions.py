1201c454865c46292cfe122df887ca097a9f04ac
Fire Chicken <firechickenproductivity@gmail.com>
2025-08-06 17:56:20 -0600

    Define described function

---
+++ core/described_functions.py
@@ -0,0 +1,28 @@
+# This file provides support for defining functions that have an associated description
+# The talon version of python does not have adequate inspection support to get the doc string of a function, so this serves as an alternative
+
+from talon import Module, actions
+
+class DescribedFunction:
+	def __init__(self, function, description: str):
+		self.function = function
+		self.description = description
+
+	def __call__(self, *args, **kwargs):
+		return self.function(*args, **kwargs)
+
+	def __repr__(self):
+		return self.description
+
+mod = Module()
+@mod.action_class
+class Actions:
+	def described_function_create(function, description: str):
+		"""Create a function with an associated description"""
+		return DescribedFunction(function, description)
+
+	def described_function_create_insert_between(before: str, after: str):
+		"""Creates a described function for calling actions.user.insert_between"""
+		return DescribedFunction(actions.user.insert_between, f"actions.user.insert_between('{before}', '{after}')")
+		
+		
