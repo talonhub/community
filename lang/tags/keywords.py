59c5437620abf0157601b948e8972a9132d057b4
Samuel Dianda <107892169+FireChickenProductivity@users.noreply.github.com>
2025-08-09 13:25:22 -0600

    Rename user.code_keyword_bare to user.code_keyword_unprefixed (#1969)

--- lang/tags/keywords.py
+++ lang/tags/keywords.py
@@ -6,12 +6,12 @@ mod.tag("code_keywords", desc="Tag for enabling commands for keywords")
 
 mod.list("code_keyword", desc="List of keywords for active language")
 mod.list(
-    "code_keyword_bare",
+    "code_keyword_unprefixed",
     desc="List of keywords for active language that can be dictated by name alone or the put (name) command",
 )
 
 
-@mod.capture(rule=("{user.code_keyword}|{user.code_keyword_bare}"))
+@mod.capture(rule=("{user.code_keyword}|{user.code_keyword_unprefixed}"))
 def code_keyword(m) -> str:
     return str(m)
 
