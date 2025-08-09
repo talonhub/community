acf280e607358b1d55704bb84272db425a24d090
Samuel Dianda <107892169+FireChickenProductivity@users.noreply.github.com>
2025-08-09 10:59:15 -0600

    Better handle keyword spaces (#1946)
    Better handle keyword spaces with the keyword grammar. Addresses part of
    issue #1942.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Nicholas Riley <com-github@sabi.net>

--- lang/tags/keywords.py
+++ lang/tags/keywords.py
@@ -20,5 +20,16 @@ def code_keyword(m) -> str:
 class Actions:
     def code_keyword(keywords: list[str]):
         """Adds keywords"""
-        for keyword in keywords:
-            actions.insert(keyword)
+        if len(keywords) == 1:
+            actions.insert(keywords[0])
+        else:
+            # every keyword is separated by a space
+            # the spacing before the first keyword and after the last keyword is kept
+            leading_word = keywords[0].rstrip()
+            trailing_word = keywords[-1].lstrip()
+            stripped_words = (
+                [leading_word]
+                + [keyword.strip() for keyword in keywords[1:-1]]
+                + [trailing_word]
+            )
+            actions.insert(" ".join(stripped_words))
