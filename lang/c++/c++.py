cea2c7ddb6aa776e77a796b9b05784abd3d70f56
Fire Chicken <firechickenproductivity@gmail.com>
2025-08-11 20:39:48 -0600

    Provide c++ operators definition

---
+++ lang/c++/c++.py
@@ -0,0 +1,15 @@
+from talon import Module, Context
+
+from ..c.c import operators
+from ..tags.operators import Operators
+
+ctx = Context()
+
+ctx.matches = r"""
+code.language: cpp
+"""
+
+@ctx.action_class("user")
+class UserActions:
+	def code_get_operators() -> Operators:
+		return operators
