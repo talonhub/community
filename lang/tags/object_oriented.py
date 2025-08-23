dc911cf2e14bb9038ecf4a56c32efbf67418af12
Samuel Dianda <107892169+FireChickenProductivity@users.noreply.github.com>
2025-08-23 17:16:56 -0600

    Add python common member functions (#1997)
    Expand the javascript common member functions into a general grammar and
    add python support. I also added a help command for the common member
    functions.
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Nicholas Riley <com-github@sabi.net>

--- lang/tags/object_oriented.py
+++ lang/tags/object_oriented.py
@@ -8,6 +8,8 @@ mod.tag(
     desc="Tag for enabling basic object oriented programming commands (objects, classes, etc)",
 )
 
+mod.list("code_common_method", desc="Commonly invoked method, e.g. 'foo' in '.foo()'")
+
 
 @mod.action_class
 class Actions:
@@ -20,3 +22,7 @@ class Actions:
     def code_define_class():
         """Starts a class definition (e.g., Java's "class" keyword)"""
         actions.user.insert_snippet_by_name("classDeclaration")
+
+    def code_method(name: str):
+        """Inserts a method invocation"""
+        actions.user.insert_between(f".{name}(", ")")
