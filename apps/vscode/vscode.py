8b2834f052c37b59e120bc5e3a385c317da51d06
Fire Chicken <firechickenproductivity@gmail.com>
2025-08-23 09:18:16 -0600

    Limit to when the editor is focused

--- apps/vscode/vscode.py
+++ apps/vscode/vscode.py
@@ -136,6 +136,14 @@ class EditActions:
         else:
             actions.user.vscode("actions.find")
 
+@ctx_editor.action_class("user")
+class EditorUserActions:
+    def insert_between(before: str, after: str):
+        """Use the snippet system to directly insert the text around the cursor"""
+        escaped_before = actions.user.escape_snippet_stops(before)
+        escaped_after = actions.user.escape_snippet_stops(after)
+        snippet = f"{escaped_before}$0{escaped_after}"
+        actions.user.insert_snippet(snippet)
 
 @ctx.action_class("edit")
 class EditActions:
@@ -436,10 +444,3 @@ class UserActions:
 
     def move_cursor_to_next_snippet_stop():
         actions.user.vscode("jumpToNextSnippetPlaceholder")
-
-    def insert_between(before: str, after: str):
-        """Use the snippet system to directly insert the text around the cursor"""
-        escaped_before = actions.user.escape_snippet_stops(before)
-        escaped_after = actions.user.escape_snippet_stops(after)
-        snippet = f"{escaped_before}$0{escaped_after}"
-        actions.user.insert_snippet(snippet)
