7719d2b086679db37bdb00accf4f75f52caa1530
Fire Chicken <firechickenproductivity@gmail.com>
2025-08-26 09:42:04 -0600

    Update snippet with phrase

--- core/snippets/snippets.talon
+++ core/snippets/snippets.talon
@@ -2,6 +2,7 @@
 snip {user.snippet}: user.insert_snippet_by_name_with_stop_at_end(snippet)
 
 snip {user.snippet_with_phrase} <user.text>:
-    user.insert_snippet_by_name_with_phrase(snippet_with_phrase, text)
+    #user.insert_snippet_by_name_with_phrase(snippet_with_phrase, text)
+    user.insert_snippet_by_name_with_phrase_and_stop_at_end(snippet_with_phrase, text)
 
 snip next: user.move_cursor_to_next_snippet_stop()
--- core/snippets/snippets_insert.py
+++ core/snippets/snippets_insert.py
@@ -41,21 +41,17 @@ class Actions:
     def insert_snippet_by_name_with_phrase(name: str, phrase: str):
         """Insert snippet <name> with phrase <phrase>"""
         snippet: Snippet = actions.user.get_snippet(name)
-        substitutions = {}
+        substitutions = compute_phrase_substitutions(snippet, phrase)
+        body = compute_snippet_body_with_substitutions(snippet, substitutions)
+        actions.user.insert_snippet(body)
 
-        for variable in snippet.variables:
-            if variable.insertion_formatters is not None:
-                formatters = ",".join(variable.insertion_formatters)
-                formatted_phrase = actions.user.formatted_text(phrase, formatters)
-                substitutions[variable.name] = formatted_phrase
-
-        if not substitutions:
-            raise ValueError(
-                f"Can't use snippet phrase. No variable with insertion formatter in snippet '{name}'"
-            )
-
-        actions.user.insert_snippet_by_name(name, substitutions)
-
+    def insert_snippet_by_name_with_phrase_and_stop_at_end(name: str, phrase: str):
+        """Insert snippet <name> with phrase <phrase> and a stop at the end"""
+        snippet: Snippet = actions.user.get_snippet(name)
+        substitutions = compute_phrase_substitutions(snippet, phrase)
+        body = compute_snippet_body_with_substitutions(snippet, substitutions)
+        body = add_final_stop_to_snippet_body(body)
+        actions.user.insert_snippet(body)
 
 def compute_snippet_body_with_substitutions(
     snippet: Snippet, substitutions: dict[str, str]
@@ -70,3 +66,19 @@ def compute_snippet_body_with_substitutions(
                 )
             body = reg.sub(v, body)
     return body
+
+def compute_phrase_substitutions(snippet: Snippet, phrase: str):
+    substitutions = {}
+
+    for variable in snippet.variables:
+        if variable.insertion_formatters is not None:
+            formatters = ",".join(variable.insertion_formatters)
+            formatted_phrase = actions.user.formatted_text(phrase, formatters)
+            substitutions[variable.name] = formatted_phrase
+
+    if not substitutions:
+        raise ValueError(
+            f"Can't use snippet phrase. No variable with insertion formatter in snippet '{snippet.name}'"
+        )
+    
+    return substitutions
