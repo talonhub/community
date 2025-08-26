29c3bbcd623b1fdccc179541af64f548357fe8c7
Fire Chicken <firechickenproductivity@gmail.com>
2025-08-26 10:51:30 -0600

    Simplify a little

--- core/snippets/snippets_parser.py
+++ core/snippets/snippets_parser.py
@@ -165,20 +165,20 @@ def add_final_stop_to_snippet_body(body: str) -> str:
 
     biggest_variable_number: int | None = find_largest_variable_number(body)
     if biggest_variable_number is not None:
-        biggest_variable_matches = find_variable_matches(
-            str(biggest_variable_number), body
-        )
         # If the biggest matching variable is at the end and there is no zero stop, make no change
         if (
-            len(biggest_variable_matches) > 0
-            and biggest_variable_matches[-1].end() == len(body)
-            and len(final_stop_matches) == 0
+            len(final_stop_matches) == 0 and 
+            is_variable_last_match_at_end(str(biggest_variable_number), body)
         ):
             return body
         replacement_name = str(biggest_variable_number + 1)
         body = replace_final_stop(body, replacement_name, final_stop_matches)
     body += "$0"
     return body
+
+def is_variable_last_match_at_end(variable: str, body) -> bool:
+    matches = find_variable_matches(variable, body)
+    return len(matches) > 0 and matches[-1].end() == len(body)
 
 
 def replace_final_stop(body: str, replacement_name: str, final_stop_matches) -> str:
