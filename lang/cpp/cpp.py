c2b50def64f4889ed011fa023f45bff71dab2e58
Fire Chicken <firechickenproductivity@gmail.com>
2025-08-29 13:49:12 -0600

    Improve type support

--- lang/cpp/cpp.py
+++ lang/cpp/cpp.py
@@ -10,11 +10,19 @@ ctx.matches = r"""
 code.language: cpp
 """
 
+mod.list("cpp_standard_type", desc="Types from the cplusplus standard library")
+mod.list("cpp_standard_prefix", desc="Prefixes for referring to the standard library")
+
+@mod.capture(rule="{user.cpp_standard_prefix} {user.cpp_standard_type}")
+def cpp_standard_type(m) -> str:
+    # Discard the prefix word and properly prefix the type
+    return "std::" + m[1]
 
 @ctx.action_class("user")
 class UserActions:
     def code_get_operators() -> Operators:
         return operators
 
-mod.list("cpp_standard_type", desc="Types from the cplusplus standard library")
-mod.list("cpp_standard_prefix", desc="Prefixes for referring to the standard library")
+@ctx.capture("user.code_type", rule="<user.c_types> | <user.cpp_standard_type>")
+def code_type(m):
+    return m[0]
--- lang/cpp/cpp.talon
+++ lang/cpp/cpp.talon
@@ -8,3 +8,6 @@ tag(): user.code_operators_math
 tag(): user.code_operators_pointer
 
 {user.cpp_standard_prefix} {user.cpp_standard_type}: insert("std::{cpp_standard_type}")
+
+type <user.code_type>: insert(code_type)
+ 
