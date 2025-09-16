1fb667da8af9fe1cb27d03b7499519c4ef3bdf60
Fire Chicken <firechickenproductivity@gmail.com>
2025-09-16 11:50:01 -0600

    Stop using self

--- lang/c/c.py
+++ lang/c/c.py
@@ -18,30 +18,30 @@ code.language: c
 code.language: cpp
 """
 
-c_and_cpp_ctx.lists["self.c_pointers"] = {
+c_and_cpp_ctx.lists["user.c_pointers"] = {
     "pointer": "*",
     "pointer to pointer": "**",
 }
 
-c_and_cpp_ctx.lists["self.stdint_signed"] = {
+c_and_cpp_ctx.lists["user.stdint_signed"] = {
     "signed": "",
     "unsigned": "u",
     "you": "u",
 }
 
-c_and_cpp_ctx.lists["self.c_type_bit_width"] = {
+c_and_cpp_ctx.lists["user.c_type_bit_width"] = {
     "eight": "8",
     "sixteen": "16",
     "thirty two": "32",
     "sixty four": "64",
 }
 
-c_and_cpp_ctx.lists["self.c_signed"] = {
+c_and_cpp_ctx.lists["user.c_signed"] = {
     "signed": "signed",
     "unsigned": "unsigned",
 }
 
-c_and_cpp_ctx.lists["self.stdint_types"] = {
+c_and_cpp_ctx.lists["user.stdint_types"] = {
     "character": "int8_t",
     "char": "int8_t",
     "short": "int16_t",
@@ -58,7 +58,7 @@ c_and_cpp_ctx.lists["self.stdint_types"] = {
     "float": "float",
 }
 
-c_and_cpp_ctx.lists["self.c_types"] = {
+c_and_cpp_ctx.lists["user.c_types"] = {
     "character": "char",
     "char": "char",
     "short": "short",
@@ -106,7 +106,7 @@ mod.list("c_type_bit_width", desc="Common C type bit widths")
 
 
 # capture explicitly referenced from the C++ files
-@mod.capture(rule="(fix|fixed) [{self.stdint_signed}] [int] {self.c_type_bit_width}")
+@mod.capture(rule="(fix|fixed) [{user.stdint_signed}] [int] {user.c_type_bit_width}")
 def c_fixed_integer(m) -> str:
     """fixed-width integer types (e.g. "uint32_t")"""
     prefix = ""
@@ -115,51 +115,51 @@ def c_fixed_integer(m) -> str:
     return f"{prefix}int{m.c_type_bit_width}_t"
 
 
-@mod.capture(rule="{self.c_pointers}")
+@mod.capture(rule="{user.c_pointers}")
 def c_pointers(m) -> str:
     "Returns a string"
     return m.c_pointers
 
 
 # capture explicitly referenced from the C++ files
-@mod.capture(rule="{self.c_signed}")
+@mod.capture(rule="{user.c_signed}")
 def c_signed(m) -> str:
     "Returns a string"
     return m.c_signed
 
 
 # capture explicitly referenced from the C++ files
-@mod.capture(rule="{self.c_types}")
+@mod.capture(rule="{user.c_types}")
 def c_types(m) -> str:
     "Returns a string"
     return m.c_types
 
 
-@mod.capture(rule="{self.stdint_types}")
+@mod.capture(rule="{user.stdint_types}")
 def stdint_types(m) -> str:
     "Returns a string"
     return m.stdint_types
 
 
-@mod.capture(rule="{self.stdint_signed}")
+@mod.capture(rule="{user.stdint_signed}")
 def stdint_signed(m) -> str:
     "Returns a string"
     return m.stdint_signed
 
 
-@mod.capture(rule="[<self.c_signed>] <self.c_types> [<self.c_pointers>+]")
+@mod.capture(rule="[<user.c_signed>] <user.c_types> [<user.c_pointers>+]")
 def c_cast(m) -> str:
     "Returns a string"
     return "(" + " ".join(list(m)) + ")"
 
 
-@mod.capture(rule="[<self.stdint_signed>] <self.stdint_types> [<self.c_pointers>+]")
+@mod.capture(rule="[<user.stdint_signed>] <user.stdint_types> [<user.c_pointers>+]")
 def stdint_cast(m) -> str:
     "Returns a string"
     return "(" + "".join(list(m)) + ")"
 
 
-@mod.capture(rule="[<self.c_signed>] <self.c_types> [<self.c_pointers>]")
+@mod.capture(rule="[<user.c_signed>] <user.c_types> [<user.c_pointers>]")
 def c_variable(m) -> str:
     "Returns a string"
     return " ".join(list(m))
