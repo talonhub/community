828ef07ac1e7cf5169e9278b5c59db12fea77c8c
bluedrink9 <bluedrink9@users.noreply.github.com>
2025-11-19 16:03:55 +1300

    use actions.sleep instead of time.sleep

--- tags/terminal/readline_vi.py
+++ tags/terminal/readline_vi.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
-from time import sleep
 from typing import Callable
 
 from talon import Context, actions
+sleep = actions.sleep
 
 ctx = Context()
 ctx.matches = """
