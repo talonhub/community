9b202ee694abcc72ff67d4ca89e0ddb66bc98da1
Jeff Knaus <knaus.jeff@gmail.com>
2025-12-31 13:08:13 -0700

    basic word support

--- apps/microsoft_word/microsoft_word.py
+++ apps/microsoft_word/microsoft_word.py
@@ -1,22 +1,52 @@
 from talon import Module, Context, actions
 
 mod = Module()
+
+mod.apps.microsoft_word = r"""
+os: windows
+and app.name: Microsoft Word
+os: windows
+and app.exe: /^winword\.exe$/i
+"""
+# mod.apps.microsoft_word = r"""
+# os: windows
+# and app.name: Microsoft Edge
+# os: windows
+# and app.exe: /^msedge\.exe$/i
+# win.title: /\.docx/
+# win.title: /\.doc/
+# """
+
 ctx = Context()
-
-mod.apps.microsoft_word = r"""
-os: windows
-and app.exe: /^chrome\.exe$/i
-win.title: /\.docx/
-win.title: /\.doc/
-"""
-mod.apps.microsoft_word = r"""
-os: windows
-and app.name: Microsoft Edge
-os: windows
-and app.exe: /^msedge\.exe$/i
-win.title: /\.docx/
-win.title: /\.doc/
+ctx.matches = """
+app: microsoft_word
 """
 
-# @mod.action_class
-# class Actions:
+@ctx.action_class("user")
+class UserActions:
+    # user.pages
+    def page_current():
+        actions.key("ctrl-shift-n")
+        page = actions.edit.selected_text()
+        actions.key("tab:2 enter")
+        return int(page)
+
+    def page_next():
+        actions.key("ctrl-pagedown")
+
+    def page_previous():
+        actions.key("ctrl-pageup")
+
+    def page_jump(number: int):
+        actions.key("ctrl-g")
+        actions.insert(str(number))
+        actions.key("enter")
+
+    def page_final():
+        actions.key("ctrl-end")
+
+    # def page_rotate_right():
+    #     actions.key("shift-ctrl-0")
+
+    # def page_rotate_left():
+    #     actions.key("shift-ctrl-1")
--- apps/microsoft_word/microsoft_word.talon
+++ apps/microsoft_word/microsoft_word.talon
@@ -1,5 +1,7 @@
 app: microsoft_word
 -
+tag(): user.pages
+
 settings():
     user.context_sensitive_dictation = true
     user.paste_to_insert_threshold = 0
