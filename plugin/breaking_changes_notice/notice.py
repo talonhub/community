347f14420cb4f70fac5202c832f91ba7c33e4b75
Fire Chicken <firechickenproductivity@gmail.com>
2026-02-15 19:10:04 -0700

    Write size

--- plugin/breaking_changes_notice/notice.py
+++ plugin/breaking_changes_notice/notice.py
@@ -1,64 +1,59 @@
 # notifies the user if the breaking changes document has updated
 # this expects the breaking changes document to be two directory levels above the current one
 
-import os
-
 from talon import Module, actions, app, imgui
 
+import os
 
 @imgui.open(y=0)
 def notice_gui(gui: imgui.GUI):
-    """Notifies the user that breaking changes has changed"""
-    pass
-
+	"""Notifies the user that breaking changes has changed"""
+	pass
 
 def on_ready():
-    parent: str = os.path.dirname(__file__)
-    current_size: int = get_current_size(parent)
-    previous_size_file_name: str = "previous_breaking_changes_size"
-    previous_size_path: str = os.path.join(parent, previous_size_file_name)
-    could_read_file: bool = False
-    try:
-        previous_size: int = get_previous_size(previous_size_path)
-        could_read_file = True
-    except ValueError as ex:
-        app.notify(str(ex))
-        print(ex)
-    if could_read_file and (current_size != previous_size):
-        save_size(previous_size_path, current_size)
-        notice_gui.show()
+	parent: str = os.path.dirname(__file__)
+	current_size: int = get_current_size(parent)
+	previous_size_file_name: str = "previous_breaking_changes_size"
+	previous_size_path: str = os.path.join(parent, previous_size_file_name)
+	could_read_file: bool = True
+	try:
+		previous_size: int = get_previous_size(previous_size_path)
+	except ValueError as ex:
+		could_read_file = False
+		app.notify(str(ex))
+		print(ex)
+	except FileNotFoundError:
+		could_read_file = False
+	if could_read_file and (current_size != previous_size):
+		notice_gui.show()
+	save_size(previous_size_path, current_size)
 
 
 def get_current_size(current_directory_path: str) -> int:
-    """Gets the current size of the breaking changes file"""
-    great_grandparent: str = os.path.dirname(os.path.dirname(current_directory_path))
-    breaking_changes_path: str = os.path.join(great_grandparent, "BREAKING_CHANGES.txt")
-    stats: os.stat_result = os.stat(breaking_changes_path)
-    return stats.st_size
-
+	"""Gets the current size of the breaking changes file"""
+	great_grandparent: str = os.path.dirname(os.path.dirname(current_directory_path))
+	breaking_changes_path: str = os.path.join(great_grandparent, "BREAKING_CHANGES.txt")
+	stats: os.stat_result = os.stat(breaking_changes_path)
+	return stats.st_size
 
 def get_previous_size(path: str) -> int:
-    """Get the last read size of the breaking changes file.
-    Raises a FileNotFoundError if the size was never recorded.
-    Raises a ValueError if the value cannot be parsed"""
-    if not os.path.exists(path):
-        raise FileNotFoundError()
-    with open(path, "r") as f:
-        line_text: str = f.readline().strip()
-        try:
-            size: int = int(line_text)
-        except Exception as _:
-            raise ValueError(
-                f"Could not parse the first line of the file responsible for tracking the previous size of the breaking changes file as an integer: {line_text}"
-            )
-        return size
-
+	"""Get the last read size of the breaking changes file.
+	Raises a FileNotFoundError if the size was never recorded.
+	Raises a ValueError if the value cannot be parsed"""
+	if not os.path.exists(path):
+		raise FileNotFoundError()
+	with open(path, "r") as f:
+		line_text: str = f.readline().strip()
+		try:
+			size: int = int(line_text)
+		except Exception as _:
+			raise ValueError(f"Could not parse the first line of the file responsible for tracking the previous size of the breaking changes file as an integer: {line_text}")
+		return size
 
 def save_size(path: str, size: int):
-    """Updates the recorded previous size of the breaking changes file"""
-    with open(path, "w") as f:
-        value_text = str(size)
-        f.write(value_text)
-
+	"""Updates the recorded previous size of the breaking changes file"""
+	with open(path, "w") as f:
+		value_text = str(size)
+		f.write(value_text)
 
 app.register("ready", on_ready)
