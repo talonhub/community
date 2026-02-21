34fdc6deaf5410537723dce585b69426ab4a3315
Christian Doczkal <20443222+chdoc@users.noreply.github.com>
2026-02-21 20:31:32 +0100

    create actions to enable command and dictation mode

--- core/modes/starting_mode.py
+++ core/modes/starting_mode.py
@@ -12,13 +12,12 @@ mod.setting(
 
 def set_initial_status():
     starting_mode = settings.get("user.starting_mode", "command")
-    print(f"setting initial mode to {starting_mode}")
     if starting_mode == "sleep":
+        print("Talon ready, entering sleep mode")
         actions.speech.disable()
     elif starting_mode == "dictation":
-        actions.mode.disable("sleep")
-        actions.mode.disable("command")
-        actions.mode.enable("dictation")
+        print("Talon ready, entering dictation mode")
+        actions.user.dictation_mode()
     else:
         pass  # Talon starts in command-mode by default.
 
