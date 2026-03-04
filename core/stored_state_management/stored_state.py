6d87e973c45d7b5b544dd60191d4a8a65115a077
Fire Chicken <firechickenproductivity@gmail.com>
2026-03-03 21:06:48 -0700

    Draft shared state support

---
+++ core/stored_state_management/stored_state.py
@@ -0,0 +1,47 @@
+from talon import Module, actions, app
+
+import os, pathlib
+
+DISTANCE_TO_COMMUNITY_ROOT_DIRECTORY: int = 3
+stored_state_directory = None
+
+mod = Module()
+
+@mod.action_class
+class Actions:
+	def stored_state_does_file_exist(name: str) -> bool:
+		"""Returns if file exists with that name in the stored state directory"""
+		path = compute_stored_state_path(name)
+		return path.exists()
+
+	def stored_state_create_signal_file(name: str):
+		"""Creates an empty file with that name in the stored state directory"""
+		path = compute_stored_state_path(name)
+		with open(path, "w") as f:
+			pass
+
+	def stored_state_set_value(name: str, value: str):
+		"""Stores the value in the file with that name in the stored state directory"""
+		path = compute_stored_state_path(name)
+		with open(path, "w") as f:
+			f.write(value)
+
+def compute_stored_state_path(file_name: str):
+	"""Returns the path to the specified file in the stored state directory"""
+	if stored_state_directory is None:
+		raise ValueError("Tried to access the stored state directory before it was defined. Use the on_ready pattern if you need to use stored state on startup.")
+	return stored_state_directory / file_name
+
+	
+
+def on_ready():
+	global stored_state_directory
+	path_to_file = pathlib.Path(__file__)
+	path = path_to_file
+	for i in range(DISTANCE_TO_COMMUNITY_ROOT_DIRECTORY):
+		path = path.parent
+	stored_state_directory = path / "stored_state"
+	if not stored_state_directory.exists():
+		os.makedirs(stored_state_directory, exist_ok=True)
+
+app.register("ready", on_ready)
