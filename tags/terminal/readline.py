3ed8732fa6ae622a9e04b37d4e9191749277fd49
Alex Clarke <alexander-c@outlook.com>
2026-03-14 17:42:49 +0000

    Add compound edit commands for readline (#2152)
    Implements some compound edit commands to readline. ie can do clear word
    left, clear way left, etc
    
    It also implements copy word left and cut word left but this uses
    readline's clipboard rather than the desktops. I think this is the only
    way of doing it but might be unexpected.
    
    I also added readline support to the gnome terminal, to enable these
    commands in there
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tags/terminal/readline.py
+++ tags/terminal/readline.py
@@ -1,4 +1,6 @@
-from talon import Context, actions
+from typing import Callable
+
+from talon import Context, actions, clip
 
 ctx = Context()
 ctx.matches = r"""
@@ -32,6 +34,66 @@ class EditActions:
     #     actions.key("ctrl-y")
 
 
+# Wraps a method in a clip revert.
+# Might actually not be necessary for readline because in the default implementation it uses a seperate clipboard to the desktop
+# Though left in in case that has been modified
+def with_clip_revert(f: Callable) -> Callable:
+
+    def wrapped_method():
+        with clip.revert():
+            f()
+
+    return wrapped_method
+
+
+def with_selection_revert(f: Callable) -> Callable:
+    def wrapped_method():
+        f()
+        actions.key("ctrl-y")
+
+    return wrapped_method
+
+
+def cut_word():
+    actions.edit.word_left()
+    actions.user.cut_word_right()
+
+
+def cut_line_start():
+    actions.key("ctrl-x backspace")
+
+
+def cut_line_end():
+    actions.key("ctrl-k")
+
+
+compound_actions = {
+    # Delete
+    ("delete", "wordLeft"): with_clip_revert(actions.user.cut_word_left),
+    ("delete", "wordRight"): with_clip_revert(actions.user.cut_word_right),
+    ("delete", "word"): with_clip_revert(cut_word),
+    ("delete", "line"): with_clip_revert(actions.user.cut_line),
+    ("delete", "lineStart"): with_clip_revert(cut_line_start),
+    ("delete", "lineEnd"): with_clip_revert(cut_line_end),
+    # Copy
+    ("copyToClipboard", "wordLeft"): with_selection_revert(actions.user.cut_word_left),
+    ("copyToClipboard", "wordRight"): with_selection_revert(
+        actions.user.cut_word_right
+    ),
+    ("copyToClipboard", "word"): with_selection_revert(cut_word),
+    ("copyToClipboard", "line"): with_selection_revert(actions.user.cut_line),
+    ("copyToClipboard", "lineStart"): with_selection_revert(cut_line_start),
+    ("copyToClipboard", "lineEnd"): with_selection_revert(cut_line_end),
+    # Cut
+    ("cutToClipboard", "wordLeft"): actions.user.cut_word_left,
+    ("cutToClipboard", "wordRight"): actions.user.cut_word_right,
+    ("cutToClipboard", "word"): cut_word,
+    ("cutToClipboard", "line"): actions.user.cut_line,
+    ("cutToClipboard", "lineStart"): cut_line_start,
+    ("cutToClipboard", "lineEnd"): cut_line_end,
+}
+
+
 @ctx.action_class("user")
 class Actions:
     def cut_line():
@@ -53,16 +115,7 @@ class Actions:
         actions.user.cut_word_right()
         actions.key("ctrl-y")
 
-    # TODO: not sure how to implement these
-    # def cut_word():
-    #     pass
-    # def copy_word():
-    #     pass
-    # def delete_word():
-    #     pass
-
-    # TODO: not sure how to implement these without overwriting clipboard
-    # def delete_word_left():
-    #     actions.user.cut_word_left()
-    # def delete_word_right():
-    #     actions.user.cut_word_right()
+    def get_compound_edit_action_modifier_callback(
+        pair: tuple[str, str],
+    ) -> Callable | None:
+        return compound_actions.get(pair) or actions.next(pair)
