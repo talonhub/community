16efc3431da35e2c94b0342f1f2a8ee76c699b7c
Fire Chicken <firechickenproductivity@gmail.com>
2026-04-18 09:42:11 -0600

    Revert to multiple inserts

--- lang/tags/comment_block.py
+++ lang/tags/comment_block.py
@@ -60,13 +60,10 @@ class Actions:
 @c_like_ctx.action_class("user")
 class CActions:
     def code_comment_block(text: Optional[str] = None):
-        if text is None:
-            substitutions = None
-            snippet = f"/*\n$0\n*/"
-        else:
-            substitutions = {"0": text}
-            snippet = f"/*\n$0$1\n*/"
-        actions.user.insert_snippet_with_substitutions(snippet, substitutions)
+        actions.insert("/*\n\n*/")
+        actions.edit.up()
+        if text is not None:
+            actions.insert(text)
 
     def code_comment_block_prefix():
         actions.insert("/*")
