7e865d0d45cf0a23a39113ac159be86dc27a1807
Jeff Knaus <knaus.jeff@gmail.com>
2026-06-29 12:22:11 -0600

    Update microphone_selection_windows.py

--- plugin/microphone_selection/microphone_selection_windows.py
+++ plugin/microphone_selection/microphone_selection_windows.py
@@ -1,12 +1,12 @@
-from talon import Context, actions
+# from talon import Context, actions
 
-ctx = Context()
-ctx.matches = r"""
-user.operating_system: windows 10
-"""
+# ctx = Context()
+# ctx.matches = r"""
+# user.operating_system: windows 10
+# """
 
 
-@ctx.action_class("user")
-class Actions:
-    def microphone_toggle_video_conference():
-        actions.key("super-alt-k")
+# @ctx.action_class("user")
+# class Actions:
+#     def microphone_toggle_video_conference():
+#         actions.key("super-alt-k")
