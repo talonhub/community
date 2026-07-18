083b84ce5dddb2ae6469f0657b28337585d3c99d
Fire Chicken <firechickenproductivity@gmail.com>
2026-07-18 13:28:36 -0600

    Avoid triggering VSCode's escaping mechanisms on accident

--- apps/vscode/vscode.py
+++ apps/vscode/vscode.py
@@ -444,6 +444,8 @@ class UserActions:
         actions.key("esc")
 
     def insert_snippet(body: str):
+        # Avoid triggering VSCode's escaping mechanisms on accident
+        body = body.replace("\\", "\\\\")
         actions.user.run_rpc_command("editor.action.insertSnippet", {"snippet": body})
 
     def move_cursor_to_next_snippet_stop():
