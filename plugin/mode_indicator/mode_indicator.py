65dadef6f54cc6d7b78b69d321bbe7543f407ad4
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-09-16 11:49:54 +0200

    Updates to be compliant with Talon 3.14t

--- plugin/mode_indicator/mode_indicator.py
+++ plugin/mode_indicator/mode_indicator.py
@@ -123,6 +123,9 @@ def on_draw(c: SkiaCanvas):
     x, y = c.rect.center.x, c.rect.center.y
     radius = c.rect.height / 2 - 2
 
+    if radius <= 0:
+        return
+
     c.paint.shader = skia.Shader.radial_gradient(
         Point2d(x, y), radius, [color_mode, color_gradient]
     )
@@ -180,6 +183,7 @@ def should_show_indicator():
 def show_indicator():
     global canvas
     canvas = Canvas.from_rect(Rect(0, 0, 0, 0))
+    move_indicator()
     canvas.register("draw", on_draw)
 
 
@@ -194,7 +198,8 @@ def update_indicator():
     if should_show_indicator():
         if not canvas:
             show_indicator()
-        move_indicator()
+        else:
+            move_indicator()
         canvas.freeze()
     elif canvas:
         hide_indicator()
--- tags/terminal/readline.py
+++ tags/terminal/readline.py
@@ -33,7 +33,7 @@ class EditActions:
 # Wraps a method in a clip revert.
 # Might actually not be necessary for readline because in the default implementation it uses a seperate clipboard to the desktop
 # Though left in in case that has been modified
-def with_clip_revert(f: Callable) -> Callable:
+def with_clip_revert(f: Callable[[], None]) -> Callable[[], None]:
 
     def wrapped_method():
         with clip.revert():
@@ -42,7 +42,7 @@ def with_clip_revert(f: Callable) -> Callable:
     return wrapped_method
 
 
-def with_selection_revert(f: Callable) -> Callable:
+def with_selection_revert(f: Callable[[], None]) -> Callable[[], None]:
     def wrapped_method():
         f()
         actions.key("ctrl-y")
