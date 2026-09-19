bd7057b1712624ea043358f897455362f740264b
Fire Chicken <firechickenproductivity@gmail.com>
2026-09-18 19:17:41 -0600

    Address ui deprecation warning

--- plugin/mode_indicator/mode_indicator.py
+++ plugin/mode_indicator/mode_indicator.py
@@ -5,7 +5,7 @@ from talon.canvas import Canvas
 from talon.screen import Screen
 from talon.skia.canvas import Canvas as SkiaCanvas
 from talon.skia.imagefilter import ImageFilter
-from talon.ui import Point2d, Rect
+from skia import Point2d, Rect
 
 canvas: Canvas = None
 current_mode = ""
